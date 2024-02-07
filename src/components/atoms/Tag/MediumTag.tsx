2a9250e0e4ded7c649015e8ed0bf4b881cd8b7b8
varfaraway <farandi.963@gmail.com>
2024-02-07 08:20:23 +0700

    ACW2-1843: Button & Tag Color

--- src/components/atoms/Tag/MediumTag.tsx
+++ src/components/atoms/Tag/MediumTag.tsx
@@ -6,8 +6,8 @@ const MediumTag = ({
   title,
 }: IMediumTag) => {
   return (
-    <div className="px-2 py-1 bg-purple_light_bg rounded-sm">
-      <p className="text-purple_dark font-semibold">{title}</p>
+    <div className="px-2 py-1 bg-purple_dark/[.06] rounded-sm">
+      <p className="text-purple_dark/[.8] text-sm font-semibold">{title}</p>
     </div>
   )
 };
