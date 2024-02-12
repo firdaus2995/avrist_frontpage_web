e7d3b10c18dbd22d2fc105b73a5c71060aa69916
Firdaus <muhamad.firdaus2995@gmail.com>
2024-02-12 16:21:04 +0700

    ACW2-1790: add static image for banner

--- src/components/molecules/specifics/avram/Banner/Banner.tsx
+++ src/components/molecules/specifics/avram/Banner/Banner.tsx
@@ -1,12 +1,32 @@
+'use client';
 import React from 'react';
+import Image from 'next/image';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import BannerImg from '@/assets/images/banner.svg';
 import Button from '@/components/atoms/Button/Button';
 
-interface IBanner {
-  data: { category: string; title: string; btn: string; img: string }[];
-}
+const bannerData = [
+  {
+    category: 'Strategi Investasi',
+    title: 'Reksa Dana atau SBN?',
+    btn: 'Pelajari Lebih Lanjut',
+    img: BannerImg
+  },
+  {
+    category: 'Strategi Investasi',
+    title: 'Reksa Dana atau SBN?',
+    btn: 'Pelajari Lebih Lanjut',
+    img: BannerImg
+  },
+  {
+    category: 'Strategi Investasi',
+    title: 'Reksa Dana atau SBN?',
+    btn: 'Pelajari Lebih Lanjut',
+    img: BannerImg
+  }
+];
 
 const sliderSettings = {
   dots: true,
@@ -46,31 +66,40 @@ const sliderSettings = {
   )
 };
 
-const Banner = ({ data }: IBanner) => {
+const Banner = () => {
   return (
-    <Slider {...sliderSettings}>
-      {data.map((data, index) => (
-        <div key={index}>
-          <div
-            className="xs:h-[60vh] md:h-[40vh] bg-cover"
-            style={{ backgroundImage: `url(${data.img})` }}
-          >
-            <div className="flex flex-col md:w-[30%] md:p-20 xs:p-10 gap-4">
-              <p className="text-3xl text-bright-purple whitespace-nowrap xs:mt-20 md:mt-0">
-                {data.category}
-              </p>
-              <p className="text-3xl font-black">{data.title}</p>
-              <div>
-                <Button
-                  title={data.btn}
-                  onClick={() => console.log('Button Clicked')}
-                />
+    <div className="flex w-full">
+      <div className="w-full">
+        <Slider {...sliderSettings}>
+          {bannerData.map((data, index) => (
+            <div
+              key={index}
+              className="flex w-full xs:h-[60vh] md:h-[50vh] relative"
+            >
+              <Image
+                alt="loop-image"
+                src={data.img}
+                layout="fill"
+                objectFit="contain"
+                className="w-full h-auto xs:object-bottom md:object-right"
+              />
+              <div className="flex flex-col md:w-[30%] md:p-20 xs:p-10 gap-4 absolute z-50 top-10">
+                <p className="text-3xl text-bright-purple whitespace-nowrap xs:mt-10 md:mt-0">
+                  {data.category}
+                </p>
+                <p className="text-3xl font-black">{data.title}</p>
+                <div>
+                  <Button
+                    title={data.btn}
+                    onClick={() => console.log('Button Clicked')}
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      ))}
-    </Slider>
+          ))}
+        </Slider>
+      </div>
+    </div>
   );
 };
 
