9e0b6b8d67f164db36fa83b979d94914dcc9d04f
Firdaus <muhamad.firdaus2995@gmail.com>
2024-02-20 10:04:06 +0700

    ACW2-1848: slicing highlighted mutual fund component

--- src/app/avram/investasi/reksa-dana/page.tsx
+++ src/app/avram/investasi/reksa-dana/page.tsx
@@ -1,15 +1,53 @@
+'use client';
 import React from 'react';
-import Link from 'next/link';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import SampleImg1 from '@/assets/images/reksadana-icon-1.svg';
+import SampleImg2 from '@/assets/images/reksadana-icon-2.svg';
+import SampleImg3 from '@/assets/images/reksadana-icon-3.svg';
+import SampleImg4 from '@/assets/images/reksadana-icon-4.svg';
+import SampleImg5 from '@/assets/images/reksadana-icon-5.svg';
+import Button from '@/components/atoms/Button/Button';
+import Chart from '@/components/molecules/specifics/avram/Chart/Chart';
 
 const ReksaDana = () => {
+  const router = useRouter();
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className='h-screen flex flex-col justify-center items-center'>
-        <h1>INI ADALAH PAGE INVESTASI - REKSA DANA</h1>
-        <Link href="reksa-dana/blue-safir">Click here to view detail</Link>
+    <div className="flex flex-col items-center justify-center bg-purple_light_bg">
+      <div className="w-full md:px-32 xs:p-5 flex flex-col justify-center gap-5">
+        <div className="p-5 flex flex-row items-center justify-between rounded-xl bg-white">
+          <div className="w-32 h-32 rounded-full">
+            <Image className="h-auto w-full" src={SampleImg1} alt="sample-1" />
+          </div>
+          <div className="w-32 h-32 rounded-full">
+            <Image className="h-auto w-full" src={SampleImg2} alt="sample-2" />
+          </div>
+          <div className="w-32 h-32 rounded-full">
+            <Image className="h-auto w-full" src={SampleImg3} alt="sample-3" />
+          </div>
+          <div className="w-32 h-32 rounded-full">
+            <Image className="h-auto w-full" src={SampleImg4} alt="sample-4" />
+          </div>
+          <div className="w-32 h-32 rounded-full">
+            <Image className="h-auto w-full" src={SampleImg5} alt="sample-5" />
+          </div>
+        </div>
+        <div className="p-5 h-full flex flex-col items-center justify-center rounded-xl bg-white">
+          <div className="flex flex-col gap-2 justify-start w-full mb-5">
+            <p className="text-xl font-bold text-bright-purple">
+              Avrist Blue Safir
+            </p>
+            <p className="text-sm">Produk ini membagikan dividen</p>
+          </div>
+          <Chart />
+          <Button
+            onClick={() => router.push('reksa-dana/blue-safir')}
+            title="Pelajari Selengkapnya"
+          />
+        </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default ReksaDana;
