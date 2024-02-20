8dc23789411fffe2c61ddb3fc4a5e625ceb78bba
varfaraway <farandi.963@gmail.com>
2024-02-20 08:06:05 +0700

    ACW2-1860: Add Route to detail

--- src/app/avram/investasi/reksa-dana/page.tsx
+++ src/app/avram/investasi/reksa-dana/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Link from 'next/link';
 
 const ReksaDana = () => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className='h-screen flex justify-center items-center'>
+      <div className='h-screen flex flex-col justify-center items-center'>
         <h1>INI ADALAH PAGE INVESTASI - REKSA DANA</h1>
+        <Link href="reksa-dana/blue-safir">Click here to view detail</Link>
       </div>
     </div>
   )
