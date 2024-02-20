a7f355d289dd3b9720502fe6f545f7126a8d4c21
varfaraway <farandi.963@gmail.com>
2024-02-20 09:14:30 +0700

    ACW2-1855: Mobile Responsive

--- src/app/avram/investasi/reksa-dana/[detail]/page.tsx
+++ src/app/avram/investasi/reksa-dana/[detail]/page.tsx
@@ -13,78 +13,112 @@ const ReksaDana = ({ params }: { params: { detail: string } }) => {
       </div>
 
       <div className="w-full flex flex-col self-stretch items-center justify-center px-20">
-        <div className='flex flex-col bg-white p-9 gap-6 rounded-xl'>
+        {/* WEB */}
+        <div className='xs:max-lg:hidden flex flex-col bg-white p-9 gap-6 rounded-xl'>
           <h2 className='font-bold text-2xl text-gray_body'>Fitur</h2>
           <div className='flex flex-col'>
             <div className='flex px-1 py-2 gap-2 border-b-1 border-purple_verylight'>
-              <div className='w-80 flex flex-col gap-1'>
+              <div className='w-72 flex flex-col gap-1'>
                 <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
                 <p className='text-xl text-gray_body'>10 Juni 2010  </p>
               </div>
-              <div className='w-80 flex flex-col gap-1'>
+              <div className='w-72 flex flex-col gap-1'>
                 <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
                 <p className='text-xl text-gray_body'>10 Juni 2010  </p>
               </div>
-              <div className='w-80 flex flex-col gap-1'>
+              <div className='w-72 flex flex-col gap-1'>
+                <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
+                <p className='text-xl text-gray_body'>10 Juni 2010  </p>
+              </div>
+            </div>
+            <div className='flex px-1 py-2 gap-2 border-b-1 bg-purple_dark/[.03] border-purple_verylight'>
+              <div className='w-72 flex flex-col gap-1'>
+                <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
+                <p className='text-xl text-gray_body'>10 Juni 2010  </p>
+              </div>
+              <div className='w-72 flex flex-col gap-1'>
+                <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
+                <p className='text-xl text-gray_body'>10 Juni 2010  </p>
+              </div>
+              <div className='w-72 flex flex-col gap-1'>
                 <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
                 <p className='text-xl text-gray_body'>10 Juni 2010  </p>
               </div>
             </div>
             <div className='flex px-1 py-2 gap-2 border-b-1 border-purple_verylight'>
-              <div className='w-80 flex flex-col gap-1'>
+              <div className='w-72 flex flex-col gap-1'>
                 <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
                 <p className='text-xl text-gray_body'>10 Juni 2010  </p>
               </div>
-              <div className='w-80 flex flex-col gap-1'>
+              <div className='w-72 flex flex-col gap-1'>
                 <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
                 <p className='text-xl text-gray_body'>10 Juni 2010  </p>
               </div>
-              <div className='w-80 flex flex-col gap-1'>
-                <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
-                <p className='text-xl text-gray_body'>10 Juni 2010  </p>
-              </div>
-            </div>
-            <div className='flex px-1 py-2 gap-2 border-b-1 border-purple_verylight'>
-              <div className='w-80 flex flex-col gap-1'>
-                <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
-                <p className='text-xl text-gray_body'>10 Juni 2010  </p>
-              </div>
-              <div className='w-80 flex flex-col gap-1'>
-                <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
-                <p className='text-xl text-gray_body'>10 Juni 2010  </p>
-              </div>
-              <div className='w-80 flex flex-col gap-1'>
+              <div className='w-72 flex flex-col gap-1'>
                 <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
                 <p className='text-xl text-gray_body'>10 Juni 2010  </p>
               </div>
             </div>
-            <div className='flex px-1 py-2 gap-2 border-b-1 border-purple_verylight'>
-              <div className='w-80 flex flex-col gap-1'>
+            <div className='flex px-1 py-2 gap-2 border-b-1 bg-purple_dark/[.03] border-purple_verylight'>
+              <div className='w-72 flex flex-col gap-1'>
                 <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
                 <p className='text-xl text-gray_body'>10 Juni 2010  </p>
               </div>
-              <div className='w-80 flex flex-col gap-1'>
+              <div className='w-72 flex flex-col gap-1'>
                 <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
                 <p className='text-xl text-gray_body'>10 Juni 2010  </p>
               </div>
-              <div className='w-80 flex flex-col gap-1'>
+              <div className='w-72 flex flex-col gap-1'>
                 <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
                 <p className='text-xl text-gray_body'>10 Juni 2010  </p>
               </div>
             </div>
             <div className='flex px-1 py-2 gap-2 border-purple_verylight'>
-              <div className='w-80 flex flex-col gap-1'>
+              <div className='w-72 flex flex-col gap-1'>
                 <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
                 <p className='text-xl text-gray_body'>10 Juni 2010  </p>
               </div>
-              <div className='w-80 flex flex-col gap-1'>
+              <div className='w-72 flex flex-col gap-1'>
                 <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
                 <p className='text-xl text-gray_body'>10 Juni 2010  </p>
               </div>
-              <div className='w-80 flex flex-col gap-1'>
+              <div className='w-72 flex flex-col gap-1'>
                 <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
                 <p className='text-xl text-gray_body'>10 Juni 2010  </p>
               </div>
+            </div>
+          </div>
+        </div>
+        {/* MOBILE */}
+        <div className='lg:hidden flex flex-col bg-white p-9 rounded-xl'>
+          <div className='flex px-1 py-2 gap-2 border-b-1 border-purple_verylight'>
+            <div className='w-72 flex flex-col gap-1'>
+              <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
+              <p className='text-xl text-gray_body'>10 Juni 2010  </p>
+            </div>
+          </div>
+          <div className='flex px-1 py-2 gap-2 border-b-1 bg-purple_dark/[.03] border-purple_verylight'>
+            <div className='w-72 flex flex-col gap-1'>
+              <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
+              <p className='text-xl text-gray_body'>10 Juni 2010  </p>
+            </div>
+          </div>
+          <div className='flex px-1 py-2 gap-2 border-b-1 border-purple_verylight'>
+            <div className='w-72 flex flex-col gap-1'>
+              <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
+              <p className='text-xl text-gray_body'>10 Juni 2010  </p>
+            </div>
+          </div>
+          <div className='flex px-1 py-2 gap-2 border-b-1 bg-purple_dark/[.03] border-purple_verylight'>
+            <div className='w-72 flex flex-col gap-1'>
+              <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
+              <p className='text-xl text-gray_body'>10 Juni 2010  </p>
+            </div>
+          </div>
+          <div className='flex px-1 py-2 gap-2 border-b-1 border-purple_verylight'>
+            <div className='w-72 flex flex-col gap-1'>
+              <p className='text-sm	font-bold text-purple_dark'>Tanggal Efektif</p>
+              <p className='text-xl text-gray_body'>10 Juni 2010  </p>
             </div>
           </div>
         </div>
