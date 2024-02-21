cbdf0dd571fd69220938cba09545cc6a6040dfdf
varfaraway <farandi.963@gmail.com>
2024-02-22 06:53:26 +0700

    ACW2-9999: RESTRUCTURING ATOMIC COMPOMNENT ON INVESTASI DETAIL

---
+++ src/components/molecules/specifics/avram/_investasi/FiturTableLike/index.tsx
@@ -0,0 +1,157 @@
+const FiturTableLike = () => {
+  return (
+    <div className="w-full flex flex-col self-stretch items-center justify-center px-20">
+      {/* WEB */}
+      <div className="xs:max-lg:hidden flex flex-col self-stretch bg-white p-9 gap-6 rounded-xl">
+        <h2 className="font-bold text-2xl text-gray_body">Fitur</h2>
+        <div className="flex flex-col">
+          <div className="flex px-1 py-2 gap-2 border-b-1 border-purple_verylight">
+            <div className="flex-1 flex flex-col gap-1">
+              <p className="text-sm	font-bold text-purple_dark">
+                Tanggal Efektif
+              </p>
+              <p className="text-xl text-gray_body">10 Juni 2010 </p>
+            </div>
+            <div className="flex-1 flex flex-col gap-1">
+              <p className="text-sm	font-bold text-purple_dark">
+                Tanggal Efektif
+              </p>
+              <p className="text-xl text-gray_body">10 Juni 2010 </p>
+            </div>
+            <div className="flex-1 flex flex-col gap-1">
+              <p className="text-sm	font-bold text-purple_dark">
+                Tanggal Efektif
+              </p>
+              <p className="text-xl text-gray_body">10 Juni 2010 </p>
+            </div>
+          </div>
+          <div className="flex px-1 py-2 gap-2 border-b-1 bg-purple_dark/[.03] border-purple_verylight">
+            <div className="flex-1 flex flex-col gap-1">
+              <p className="text-sm	font-bold text-purple_dark">
+                Tanggal Efektif
+              </p>
+              <p className="text-xl text-gray_body">10 Juni 2010 </p>
+            </div>
+            <div className="flex-1 flex flex-col gap-1">
+              <p className="text-sm	font-bold text-purple_dark">
+                Tanggal Efektif
+              </p>
+              <p className="text-xl text-gray_body">10 Juni 2010 </p>
+            </div>
+            <div className="flex-1 flex flex-col gap-1">
+              <p className="text-sm	font-bold text-purple_dark">
+                Tanggal Efektif
+              </p>
+              <p className="text-xl text-gray_body">10 Juni 2010 </p>
+            </div>
+          </div>
+          <div className="flex px-1 py-2 gap-2 border-b-1 border-purple_verylight">
+            <div className="flex-1 flex flex-col gap-1">
+              <p className="text-sm	font-bold text-purple_dark">
+                Tanggal Efektif
+              </p>
+              <p className="text-xl text-gray_body">10 Juni 2010 </p>
+            </div>
+            <div className="flex-1 flex flex-col gap-1">
+              <p className="text-sm	font-bold text-purple_dark">
+                Tanggal Efektif
+              </p>
+              <p className="text-xl text-gray_body">10 Juni 2010 </p>
+            </div>
+            <div className="flex-1 flex flex-col gap-1">
+              <p className="text-sm	font-bold text-purple_dark">
+                Tanggal Efektif
+              </p>
+              <p className="text-xl text-gray_body">10 Juni 2010 </p>
+            </div>
+          </div>
+          <div className="flex px-1 py-2 gap-2 border-b-1 bg-purple_dark/[.03] border-purple_verylight">
+            <div className="flex-1 flex flex-col gap-1">
+              <p className="text-sm	font-bold text-purple_dark">
+                Tanggal Efektif
+              </p>
+              <p className="text-xl text-gray_body">10 Juni 2010 </p>
+            </div>
+            <div className="flex-1 flex flex-col gap-1">
+              <p className="text-sm	font-bold text-purple_dark">
+                Tanggal Efektif
+              </p>
+              <p className="text-xl text-gray_body">10 Juni 2010 </p>
+            </div>
+            <div className="flex-1 flex flex-col gap-1">
+              <p className="text-sm	font-bold text-purple_dark">
+                Tanggal Efektif
+              </p>
+              <p className="text-xl text-gray_body">10 Juni 2010 </p>
+            </div>
+          </div>
+          <div className="flex px-1 py-2 gap-2 border-purple_verylight">
+            <div className="flex-1 flex flex-col gap-1">
+              <p className="text-sm	font-bold text-purple_dark">
+                Tanggal Efektif
+              </p>
+              <p className="text-xl text-gray_body">10 Juni 2010 </p>
+            </div>
+            <div className="flex-1 flex flex-col gap-1">
+              <p className="text-sm	font-bold text-purple_dark">
+                Tanggal Efektif
+              </p>
+              <p className="text-xl text-gray_body">10 Juni 2010 </p>
+            </div>
+            <div className="flex-1 flex flex-col gap-1">
+              <p className="text-sm	font-bold text-purple_dark">
+                Tanggal Efektif
+              </p>
+              <p className="text-xl text-gray_body">10 Juni 2010 </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* MOBILE */}
+      <div className="lg:hidden flex flex-col self-stretch bg-white p-9 rounded-xl">
+        <div className="flex px-1 py-2 gap-2 border-b-1 border-purple_verylight">
+          <div className="flex-1 flex flex-col gap-1">
+            <p className="text-sm	font-bold text-purple_dark">
+              Tanggal Efektif
+            </p>
+            <p className="text-xl text-gray_body">10 Juni 2010 </p>
+          </div>
+        </div>
+        <div className="flex px-1 py-2 gap-2 border-b-1 bg-purple_dark/[.03] border-purple_verylight">
+          <div className="flex-1 flex flex-col gap-1">
+            <p className="text-sm	font-bold text-purple_dark">
+              Tanggal Efektif
+            </p>
+            <p className="text-xl text-gray_body">10 Juni 2010 </p>
+          </div>
+        </div>
+        <div className="flex px-1 py-2 gap-2 border-b-1 border-purple_verylight">
+          <div className="flex-1 flex flex-col gap-1">
+            <p className="text-sm	font-bold text-purple_dark">
+              Tanggal Efektif
+            </p>
+            <p className="text-xl text-gray_body">10 Juni 2010 </p>
+          </div>
+        </div>
+        <div className="flex px-1 py-2 gap-2 border-b-1 bg-purple_dark/[.03] border-purple_verylight">
+          <div className="flex-1 flex flex-col gap-1">
+            <p className="text-sm	font-bold text-purple_dark">
+              Tanggal Efektif
+            </p>
+            <p className="text-xl text-gray_body">10 Juni 2010 </p>
+          </div>
+        </div>
+        <div className="flex px-1 py-2 gap-2 border-b-1 border-purple_verylight">
+          <div className="flex-1 flex flex-col gap-1">
+            <p className="text-sm	font-bold text-purple_dark">
+              Tanggal Efektif
+            </p>
+            <p className="text-xl text-gray_body">10 Juni 2010 </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FiturTableLike;
---
+++ src/components/molecules/specifics/avram/_investasi/Vendor/index.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+const Vendor = () => {
+  return (
+    <div className='w-full flex flex-col self-stretch items-center justify-center px-20'>
+      <div className='flex flex-col self-stretch bg-white rounded-xl p-9 gap-6'>
+        <p className='text-2xl text-gray_body font-bold'>Disclaimer</p>
+        <div className='flex flex-row gap-2.5'>
+          <Image alt='image_vendor' width={0} height={0} className='h-auto w-auto' src="https://i.ibb.co/9wcjf4W/Frame-315725.png" />
+          <Image alt='image_vendor' width={0} height={0} className='h-auto w-auto' src="https://i.ibb.co/1bnkJcZ/Frame-315726.png" />
+          <Image alt='image_vendor' width={0} height={0} className='h-auto w-auto' src="https://i.ibb.co/PFdfyVr/Frame-315727.png" />
+          <Image alt='image_vendor' width={0} height={0} className='h-auto w-auto' src="https://i.ibb.co/2qRx4fh/Frame-315729.png" />
+          <Image alt='image_vendor' width={0} height={0} className='h-auto w-auto' src="https://i.ibb.co/YX4S62y/Logo-Phillip-Sekuritas-Indonesia-Color-Phillip-Sekuritas-Indonesia-1.png" />
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Vendor;
