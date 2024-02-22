30921ac153fcf93f0f915dce812cd136697e08e7
varfaraway <farandi.963@gmail.com>
2024-02-22 08:50:40 +0700

    ACW2-9999: BAILED OUT ERROR

--- src/app/avram/analisis/page.tsx
+++ src/app/avram/analisis/page.tsx
@@ -4,24 +4,28 @@ interface SearchParams {
 }
 
 const Analisis: React.FC<{ searchParams: SearchParams }> = ({ searchParams }) => {
-  const tab = (searchParams.tab && !searchParams.detail) ? searchParams.tab : (!searchParams.tab && !searchParams.detail) ? 'all' : null;
-  const detail = (!searchParams.tab && searchParams.detail) ? searchParams.detail : null;
-  
-  return (
-    <div className="flex flex-col items-center justify-center gap-6 bg-purple_dark/[.03]">
-      INI ADALAH HALAMAN ANALISIS
-      {
-        detail && (
-          <div>{detail}</div>
-        )
-      }
-      {
-        tab && (
-          <div>{tab}</div>
-        )
-      }
-    </div>
-  )
+  try {
+    const tab = (searchParams.tab && !searchParams.detail) ? searchParams.tab : (!searchParams.tab && !searchParams.detail) ? 'all' : null;
+    const detail = (!searchParams.tab && searchParams.detail) ? searchParams.detail : null;
+    
+    return (
+      <div className="flex flex-col items-center justify-center gap-6 bg-purple_dark/[.03]">
+        INI ADALAH HALAMAN ANALISIS
+        {
+          detail && (
+            <div>{detail}</div>
+          )
+        }
+        {
+          tab && (
+            <div>{tab}</div>
+          )
+        }
+      </div>
+    )
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default Analisis;
--- src/app/avram/informasi/faqs/page.tsx
+++ src/app/avram/informasi/faqs/page.tsx
@@ -3,19 +3,23 @@ interface SearchParams {
 }
 
 const FAQS: React.FC<{ searchParams: SearchParams }> = ({ searchParams }) => {
-  const detail = searchParams.detail ?? null;
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-6 bg-purple_dark/[.03]">
-      {
-        detail ? (
-          <div>INI ADALAH HALAMAN FAQS</div>
-        ) : (
-          <div>INI ADALAH HALAMAN DETAIL FAQS</div>
-        )
-      }
-    </div>
-  )
+  try {
+    const detail = searchParams.detail ?? null;
+  
+    return (
+      <div className="flex flex-col items-center justify-center gap-6 bg-purple_dark/[.03]">
+        {
+          detail ? (
+            <div>INI ADALAH HALAMAN FAQS</div>
+          ) : (
+            <div>INI ADALAH HALAMAN DETAIL FAQS</div>
+          )
+        }
+      </div>
+    )    
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default FAQS;
