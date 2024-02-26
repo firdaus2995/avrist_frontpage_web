7714626d69df4665dcf3606d190894718b60d810
varfaraway <farandi.963@gmail.com>
2024-02-26 10:53:39 +0700

    ACW2-1878: Calculator

--- src/app/avram/investasi/hitung-investasi/page.tsx
+++ src/app/avram/investasi/hitung-investasi/page.tsx
@@ -1,7 +1,14 @@
+import Disclaimer from "@/components/molecules/specifics/avram/_investasi/Disclaimer";
+import Kalkulator from "@/components/molecules/specifics/avram/_investasi/Kalkulator";
+
 const HitungInvestasi = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-6 bg-purple_dark/[.03]">
-      INI ADALAH HALAMAN HITUNG INVESTASI
+      {/* KALKULATOR */}
+      <Kalkulator />
+
+      {/* DISCLAIMER */}
+      <Disclaimer />
     </div>
   )
 }
