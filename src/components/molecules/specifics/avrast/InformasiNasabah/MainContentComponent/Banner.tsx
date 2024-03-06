32e1688d264f159847f5c6e43c3d2d86ee536226
mahardicka <mahardicka404@gmail.com>
2024-03-06 07:56:00 +0700

    ACW2-1966: adjust banner cover

--- src/components/molecules/specifics/avrast/InformasiNasabah/MainContentComponent/Banner.tsx
+++ src/components/molecules/specifics/avrast/InformasiNasabah/MainContentComponent/Banner.tsx
@@ -2,7 +2,7 @@ export const Banner = () => {
   return (
     <img
       alt="banner-informasi-nasabah"
-      className="rounded-t-[80px] h-[640px] object-none relative bottom-[71px]"
+      className="rounded-t-[80px] h-[640px] object-cover relative bottom-[71px]"
       src="https://s3-alpha-sig.figma.com/img/df5a/51cf/64f6b80abd4d1448b9b41944a94b8703?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=AhxfYigwLFTFT2r2IBF8FG99qapVc7OLmf22xD7mchCYitLojG4EVwAadb41nNHnnt8FQqKcu0n-gPxA9mbGOuC0IOITWA2mwmt~dcvqyWkJcvG7qbChkUfQaOr6oS7VwFLf1OF7Khg-N0xDyFIAO6PaDWUzp00vGLeiC2p9Lyj8Z7TTBeCZcDO2Laif2nJGpRaKyi8IAEcb0Xk90T8H7uMWaYQJBzWnjRKY9KvGkeJbdOlYmh2S8Xd44HwPjw-4xJw5V0tN1TAP0WTHPa373skd6By-BVAwJ4NqXUMNP0yOMQYn5p6L9SvzAM5shtAK4XNcQJk2msYkmIswC2K4GQ__"
     />
   );
