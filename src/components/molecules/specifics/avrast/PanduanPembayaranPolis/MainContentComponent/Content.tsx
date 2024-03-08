cf68a10e028066befefdaa9dfc589143cc07630c
mahardicka <mahardicka404@gmail.com>
2024-03-09 02:00:01 +0700

    ACW2-1968: layanan/panduan-pembaayran-premi layout

---
+++ src/components/molecules/specifics/avrast/PanduanPembayaranPolis/MainContentComponent/Content.tsx
@@ -0,0 +1,14 @@
+export const Content = () => {
+  return (
+    <div className="mt-[64px]">
+      <div className="flex flex-col justify-center items-center text-center">
+        <span className="text-purple_dark font-karla font-medium text-[56px]">
+          Panduan Pembayaran Premi
+        </span>
+        <span className="font-karla font-normal text-[36px] text-gray_bold">
+          Kami memberikan Anda kemudahan dengan memilih cara pembayaran
+        </span>
+      </div>
+    </div>
+  );
+};
