223a6f8ea82ee4d732316a5220afc03111b6918b
Firdaus <muhamad.firdaus2995@gmail.com>
2024-03-19 16:37:11 +0700

    ACW2-2178|2183: detail penghargaan & karir

--- src/app/avrast/tentang-avrist-life/tentang-avrist-life/tabs/penghargaan/index.tsx
+++ src/app/avrast/tentang-avrist-life/tentang-avrist-life/tabs/penghargaan/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import BlankImage from '@/assets/images/blank-image.svg';
 import Icon from '@/components/atoms/Icon';
 import RoundedFrameBottom from '@/components/atoms/RoundedFrameBottom';
@@ -37,41 +38,48 @@ const Penghargaan = () => {
           customContent={
             <div className="grid grid-cols-2 gap-4">
               {[...Array(4)].map((_, index) => (
-                <div
+                <Link
                   key={index}
-                  className="flex flex-col gap-[18px] border border-gray_light rounded-xl text-left"
+                  href={
+                    'http://localhost:3000/avrast/tentang-avrist-life/tentang-avrist-life/tabs/penghargaan/detail'
+                  }
                 >
-                  <Image
-                    alt="blank-image"
-                    width={0}
-                    height={170}
-                    src={BlankImage}
-                    className="w-auto rounded-t-[12px]"
-                  />
-                  <div className="flex flex-col gap-2 p-5">
-                    <p className="text-xs">10 January 2023</p>
-                    <p className="text-[20px] font-bold">
-                      Best Mutual Fund Awards 2023
-                    </p>
-                    <p className="text-[20px]">Reksa Dana Terbaik 2023</p>
-                    <p className="text-xs">
-                      Avrist IDX30 (Reksa Dana Indeks & ETF Pasif Periode 5
-                      Tahun) & Avrist Ada Kas Mutiara (Reksa Dana Pasar Uang
-                      Periode 5 Tahun Aset di Atas Rp10 Miliar - Rp100 Miliar)
-                    </p>
-                    <div className="flex flex-row items-start gap-1 text-left">
-                      <p className="text-purple_dark font-bold text-sm cursor-pointer text-left">
-                        Baca Berita Pers
+                  <div
+                    key={index}
+                    className="flex flex-col gap-[18px] border border-gray_light rounded-xl text-left"
+                  >
+                    <Image
+                      alt="blank-image"
+                      width={0}
+                      height={170}
+                      src={BlankImage}
+                      className="w-auto rounded-t-[12px]"
+                    />
+                    <div className="flex flex-col gap-2 p-5">
+                      <p className="text-xs">10 January 2023</p>
+                      <p className="text-[20px] font-bold">
+                        Best Mutual Fund Awards 2023
                       </p>
-                      <Icon
-                        width={16}
-                        height={16}
-                        name="chevronRight"
-                        color="purple_dark"
-                      />
+                      <p className="text-[20px]">Reksa Dana Terbaik 2023</p>
+                      <p className="text-xs">
+                        Avrist IDX30 (Reksa Dana Indeks & ETF Pasif Periode 5
+                        Tahun) & Avrist Ada Kas Mutiara (Reksa Dana Pasar Uang
+                        Periode 5 Tahun Aset di Atas Rp10 Miliar - Rp100 Miliar)
+                      </p>
+                      <div className="flex flex-row items-start gap-1 text-left">
+                        <p className="text-purple_dark font-bold text-sm cursor-pointer text-left">
+                          Baca Berita Pers
+                        </p>
+                        <Icon
+                          width={16}
+                          height={16}
+                          name="chevronRight"
+                          color="purple_dark"
+                        />
+                      </div>
                     </div>
                   </div>
-                </div>
+                </Link>
               ))}
             </div>
           }
