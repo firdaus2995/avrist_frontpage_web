b7c6e36f9954f09bddb075b400566fd144bbfa20
mahardicka <mahardicka404@gmail.com>
2024-04-14 14:46:09 +0700

    ACW2-2630: syariah product detail integration

--- src/services/content-page.api.ts
+++ src/services/content-page.api.ts
@@ -1,14 +1,31 @@
 import { notFound } from 'next/navigation';
+import { ContentResponse } from '@/types/content.type';
 import { PageResponse } from '@/types/page.type';
-import { httpService } from '@/utils/httpService';
+import { QueryParams, httpService } from '@/utils/httpService';
 
 export const getContentPage = async (slug: string) => {
   return await httpService<PageResponse>('page', slug, { method: 'GET' });
 };
 
+export const getContent = async (slug: string, query: QueryParams) => {
+  return await httpService<ContentResponse>('content', slug, {
+    method: 'GET',
+    queryParams: query
+  });
+};
+
 export const handleGetContentPage = async (slug: string) => {
   try {
     const data = await getContentPage(slug);
+    return data;
+  } catch (error) {
+    return notFound();
+  }
+};
+
+export const handleGetContent = async (slug: string, query: QueryParams) => {
+  try {
+    const data = await getContent(slug, query);
     return data;
   } catch (error) {
     return notFound();
