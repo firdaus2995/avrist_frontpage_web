304496586ec077b30e6e4f4503696734946e5b1f
javierdimastri <hilmantrihatmojo@gmail.com>
2024-04-22 21:33:32 +0700

    ACW2-2502: Handle subcard video ACW2-2704

--- src/components/molecules/specifics/avrast/Klaim/PanduanKlaim/components/VideoCards/SubCard/index.tsx
+++ src/components/molecules/specifics/avrast/Klaim/PanduanKlaim/components/VideoCards/SubCard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Image from 'next/image';
 import { VideoItem } from '../../../types';
 import PlayButton from '@/assets/images/play-button.svg';
@@ -9,6 +9,26 @@ type SubCardProps = {
 };
 
 const SubCard: React.FC<SubCardProps> = ({ item, onClick }) => {
+  
+  const videoId = useMemo(() => {
+    if (!item.videoUrl) return '';
+    const splittedUrl = item.videoUrl.split('/');
+    const lastPiece = splittedUrl.at(-1);
+
+    if (lastPiece && lastPiece.includes('watch')) {
+      const anotherSplitted = lastPiece.split('?v=');
+      return anotherSplitted.at(-1) ?? '';
+    } else if (lastPiece && lastPiece.includes('?si=')) {
+      const anotherSplitted = lastPiece.split('?si=');
+      return anotherSplitted.at(0);
+    } 
+    else if (lastPiece && lastPiece.includes('?')) {
+      const videoIdParam = lastPiece.split('?')[0];      
+      return videoIdParam ?? '';
+    }
+    return lastPiece ?? '';
+  }, [item.videoUrl]);
+
   return (
     <div
       onClick={() => onClick(item.id)}
@@ -20,7 +40,7 @@ const SubCard: React.FC<SubCardProps> = ({ item, onClick }) => {
           width={0}
           height={0}
           className="absolute w-full h-full inset-0"
-          src={item.videoThumbnail}
+          src={item.videoThumbnail || `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`}
         />
         <div className="w-full h-full absolute flex items-center justify-center">
           <Image alt={'play-button'} className="w-16" src={PlayButton} />
