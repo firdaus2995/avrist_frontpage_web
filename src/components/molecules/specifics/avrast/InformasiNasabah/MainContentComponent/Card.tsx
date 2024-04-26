8ef38977da749763248899e3ffade73c71f3adf9
javierdimastri <hilmantrihatmojo@gmail.com>
2024-04-26 22:34:51 +0700

    ACW2-2716: Handle wording

--- src/components/molecules/specifics/avrast/InformasiNasabah/MainContentComponent/Card.tsx
+++ src/components/molecules/specifics/avrast/InformasiNasabah/MainContentComponent/Card.tsx
@@ -12,27 +12,27 @@ import PERSON from '@/assets/images/common/person.svg';
 const mockData = [
   {
     id: '1',
-    title: 'Kelola Polis',
-    desc: 'Perkembangan informasi dan keputusan secara jelas, efektif dan transparansi.',
+    title: 'Panduan polis Anda',
+    desc: 'Lihat detail panduan polis yang anda miliki di sini',
     icon: CONTACTS,
-    btn: 'Lihat Panduan',
+    btn: 'Lihat Di Sini',
     link: 'layanan/kelola-polis'
   },
   {
     id: '2',
-    title: 'Penanganan Pengaduan',
+    title: 'Mekanisme Penanganan Pengaduan',
     desc: 'Kami menghargai dan mendengarkan saran dan keluhan dari nasabah Kami.',
     icon: DOCUMENT_SEARCH,
-    btn: 'Lihat Prosedur',
+    btn: 'Selengkapnya',
     link: 'layanan/penanganan-pengaduan'
   },
   {
     id: '3',
-    title: 'Agen Aktif',
-    desc: 'Temukan agen asuransi dan dapatkan layanan asuransi sesuai kebutuhan Anda',
+    title: 'Daftar Tenaga Pemasar',
+    desc: 'Temukan daftar tenaga pemasar aktif Avrist Assurance',
     icon: PERSON,
-    btn: 'Lihat Agen',
-    link: ''
+    btn: 'Temukan Di sini',
+    link: 'layanan/penanganan-pengaduan'
   }
 ];
 
@@ -42,13 +42,12 @@ export const ContentCard = () => {
       {mockData.map((i) => (
         <CardPurple key={i.id}>
           <div className="flex flex-col items-center m-[24px]">
-            {/* <Image src={i.icon} height={100} width={100} alt="icon" /> */}
-            <Image
+          <Image
             alt={i.toString()}
             src={i.icon}
             className="w-[100px] h-[100px]"
             />
-            <p className="text-center font-bold text-[32px] font-karla mt-[24px]">
+            <p className="text-center font-bold text-[28px] font-karla mt-[24px]">
               {i.title}
             </p>
             <p className="font-opensans text-[16px] text-center mt-[12px]">
