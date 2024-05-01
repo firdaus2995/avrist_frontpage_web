40031ac855718b903e59246959db1f48eef9d018
Mahardicka <mahardicka.nur@sinbad.co.id>
2024-05-02 00:56:16 +0700

    ACW2-2771: integration dplk product list

--- src/app/avrist-dplk/produk/page.tsx
+++ src/app/avrist-dplk/produk/page.tsx
@@ -1,107 +1,53 @@
-'use client';
-import HeroDplk2 from '@/assets/images/avrast/dplk/hero-dplk-2.svg';
-import YellowHome from '@/assets/images/avrast/dplk/yellow-dplk-home-sun.svg';
-import BlankImage from '@/assets/images/blank-image.svg';
+import { Suspense } from 'react';
 
+import DPLKProductList from '../DPLKProduct';
 import ProdukClaim from '@/assets/images/produk-claim.svg';
 import ProdukPolis from '@/assets/images/produk-polis.svg';
 import ProdukRumahSakit from '@/assets/images/produk-rumah-sakit.svg';
 import ProdukTestimoni from '@/assets/images/produk-testimoni.svg';
 
-import Icon from '@/components/atoms/Icon';
 import RoundedFrameBottom from '@/components/atoms/RoundedFrameBottom';
 import RoundedFrameTop from '@/components/atoms/RoundedFrameTop';
 import HelpCard from '@/components/molecules/specifics/avrast/Cards/HelpCard';
-import CardProduct from '@/components/molecules/specifics/avrast/Cards/ProductCard';
-import CategoryPills from '@/components/molecules/specifics/avrast/CategoryPills';
-import CategoryPillsBox from '@/components/molecules/specifics/avrast/CategoryPillsBox';
 import SimpleContainer from '@/components/molecules/specifics/avrast/Containers/Simple';
 import FooterCards from '@/components/molecules/specifics/avrast/FooterCards';
 import Hero from '@/components/molecules/specifics/avrast/Hero';
-import SearchBar from '@/components/molecules/specifics/avrast/SearchBar';
 
-const ProdukDplk = () => {
+import {
+  handleGetContentPage,
+  handleGetContent
+} from '@/services/content-page.api';
+import {
+  pageTransformer,
+  singleImageTransformer
+} from '@/utils/responseTransformer';
+
+const ProdukDplk = async () => {
+  const pageBase = await handleGetContentPage('halaman-produk-dplk');
+  const { content } = pageTransformer(pageBase);
+  const titleImage = singleImageTransformer(content['title-image']);
+  const bannerImage = singleImageTransformer(content['banner-image']);
+  const cta1Image = singleImageTransformer(content['cta1-image']);
+
+  const contentBase = await handleGetContent('Produk-Avrast-DPLK', {});
+  const products = contentBase.data.contentDataList;
+
   return (
-    <div>
+    <Suspense fallback={null}>
       <Hero
         title="Produk DPLK"
         breadcrumbsData={[
           { title: 'Beranda', href: '/' },
-          { title: 'Avrist DPLK', href: '/avrist-dplk' },
-          { title: 'Produk DPLK', href: '/avrist-dplk/produk' }
+          {
+            title: 'Avrist Syariah',
+            href: '/avrist-syariah?tab=Tentang+Avrist+Syariah'
+          },
+          { title: 'Produk DPLK', href: '#' }
         ]}
-        bottomImage={HeroDplk2}
+        bottomImage={bannerImage.imageUrl}
+        imageUrl={titleImage.imageUrl}
       />
-      <SimpleContainer>
-        <CategoryPills
-          buttonTitle={[
-            'Tentang Avrist DPLK',
-            'Dewan Pengawas DPLK',
-            'Manfaat Utama',
-            'Produk',
-            'Klaim & Layanan'
-          ]}
-          selectedCategory="Produk"
-          buttonActiveClassname="bg-dplk_yellow border-dplk_yellow"
-          buttonInactiveClassname="bg-transparent border-dplk_yellow text-black hover:bg-dplk_yellow hover:border-dplk_yellow hover:text-white"
-          buttonActiveTextClassname="text-white"
-          links={{
-            'Tentang Avrist DPLK': '/avrist-dplk/#TentangAvristDPLK',
-            'Dewan Pengawas DPLK': '/avrist-dplk/#DewanPengawasDPLK',
-            'Manfaat Utama': '/avrist-dplk/#ManfaatUtama',
-            Produk: '/avrist-dplk/produk',
-            'Klaim & Layanan': '/avrist-dplk/klaim-layanan'
-          }}
-        />
-        <div className="flex">
-          <div className="w-1/2">
-            <CategoryPillsBox
-              buttonTitle={['Layanan Employer', 'Layanan Employee']}
-              buttonClassname="accent-dplk_yellow border-dplk_yellow"
-              buttonTextClassname="text-black"
-            />
-          </div>
-          <div className="w-1/2">
-            <SearchBar
-              placeholder="Cari Produk"
-              searchButtonTitle="Cari"
-              searchButtonClassname="bg-dplk_yellow text-white"
-            />
-          </div>
-        </div>
-        <div className="grid grid-cols-3 gap-[24px]">
-          {[...Array(6)].map((_, index) => (
-            <CardProduct
-              key={index}
-              symbol={YellowHome}
-              title="Avrist DPLK"
-              summary="Lorem Ipsum"
-              description="Lorem ipsum dolor sit amet consectetur purus tortor praesent feugiat ultricies aliquam lacinia pretium potenti."
-              tags={['Avrist Syariah', 'Premi Tetap', 'Premi Berkala']}
-              cardClassname="bg-white border-b-dplk_yellow"
-              cardTitleClassname="text-dplk_yellow"
-              cardTagsClassname="bg-dplk_yellow/[.2] text-dplk_yellow"
-              cardButtonClassname="bg-dplk_yellow text-white"
-            />
-          ))}
-        </div>
-        <div className="flex flex-col gap-4 sm:flex-row justify-between">
-          <div>
-            <p className="text-[20px]">
-              Menampilkan{' '}
-              <span className="font-bold text-dplk_yellow">1-9</span> dari{' '}
-              <span className="font-bold">20</span> hasil
-            </p>
-          </div>
-          <div className="flex flex-row gap-[8px] items-center">
-            <p className="text-[20px] text-dplk_yellow font-bold">1</p>
-            <p className="text-[20px]">2</p>
-            <p className="text-[20px]">3</p>
-            <p className="text-[20px]">4</p>
-            <Icon name="chevronRight" color="dplk_yellow" />
-          </div>
-        </div>
-      </SimpleContainer>
+      <DPLKProductList products={products} />
       <RoundedFrameBottom bgColor="bg-white" frameColor="bg-white" />
       <SimpleContainer>
         <HelpCard
@@ -115,7 +61,8 @@ const ProdukDplk = () => {
           buttonClassname="bg-white border border-white"
           buttonTextClassname="text-dplk_yellow"
           buttonTitle="Tanya Avrista"
-          image={BlankImage}
+          href="/tanya-avrista"
+          image={cta1Image.imageUrl}
         />
       </SimpleContainer>
       <RoundedFrameTop bgColor="bg-white" frameColor="bg-white" />
@@ -123,26 +70,31 @@ const ProdukDplk = () => {
         cards={[
           {
             title: 'Rumah Sakit Rekanan',
-            icon: ProdukRumahSakit
+            icon: ProdukRumahSakit,
+            subtitle: 'Lebih Lanjut',
+            href: '/klaim-layanan/layanan?tab=Rumah+Sakit+Rekanan'
           },
           {
             title: 'Klaim & Layanan',
             icon: ProdukClaim,
-            subtitle: 'Lebih Lanjut'
+            subtitle: 'Lebih Lanjut',
+            href: '/avrist-syariah/klaim-layanan'
           },
           {
             title: 'Kelola Polis',
             icon: ProdukPolis,
-            subtitle: 'Login Akun'
+            subtitle: 'Login Akun',
+            href: '/klaim-layanan/layanan/kelola-polis'
           },
           {
             title: 'Testimonial',
             icon: ProdukTestimoni,
-            subtitle: 'Lebih Lanjut'
+            subtitle: 'Lebih Lanjut',
+            href: '/promo-berita/berita?tab=Testimonial'
           }
         ]}
       />
-    </div>
+    </Suspense>
   );
 };
 
