ffec89d899e1212ed430cdcd138a86032ec8ae94
Firdaus <muhamad.firdaus2995@gmail.com>
2024-05-13 02:47:11 +0700

    ACW2-2994: bugfix ukuran gap teks dengan field search pada section banner

--- src/components/molecules/specifics/avrast/TanyaAvrista/SearchTerm/index.tsx
+++ src/components/molecules/specifics/avrast/TanyaAvrista/SearchTerm/index.tsx
@@ -4,19 +4,21 @@ import React from 'react';
 import Image from 'next/image';
 import Input from '@/components/atoms/Input';
 
-const SearchTerm = ({ bannerImage } : { bannerImage: string }) => {
+const SearchTerm = ({ bannerImage }: { bannerImage: string }) => {
   return (
     <div className="z-[1] w-full bg-purple_dark -mt-1">
       <div className="w-full h-[640px] flex items-center absolute">
         <div className="w-full h-full grid grid-cols-2">
           <span />
-          <div className="flex flex-col md:justify-center sm:gap-1 md:gap-4 md:ml-20 sm:ml-0">
-            <p className="font-karla md:text-[48px] 2xl:text-5xl font-extrabold text-white">
-              Halo, apa kabar?
-            </p>
-            <p className="font-karla md:text-[48px] 2xl:text-5xl text-white">
-            Bagaimana Avrista bisa membantumu?
-            </p>
+          <div className="flex flex-col md:justify-center sm:gap-1 md:gap-[36px] md:ml-20 sm:ml-0">
+            <div>
+              <p className="font-karla md:text-[48px] 2xl:text-5xl font-extrabold text-white">
+                Halo, apa kabar?
+              </p>
+              <p className="font-karla md:text-[48px] 2xl:text-5xl text-white">
+                Bagaimana Avrista bisa membantumu?
+              </p>
+            </div>
             <div className="w-full sm:mt-2 mt-6">
               <Input
                 customInputClass="md:w-[60%] 2xl:w-[72%] grow !bg-gray_bglightgray !border-none"
