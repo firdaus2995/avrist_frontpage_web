f4306c5ff36b0dd341879a1bf7670dfbf396f80b
Firdaus <muhamad.firdaus2995@gmail.com>
2024-05-13 15:05:39 +0700

    ACW2-2723: tambahan untuk halaman detail produk korporasi

--- src/app/api/produk/korporasi/route.ts
+++ src/app/api/produk/korporasi/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 import { getContentPage } from "@/services/content-page.api";
 
 export async function GET() {
-    const data = await getContentPage('produk-individu-hlm')
+    const data = await getContentPage('produk-korporasi')
     return NextResponse.json(data, { status: 200 });
 }
