714dd1a4ca7bcdf6b9d0fe88d5d49f71bf9018c1
Firdaus <muhamad.firdaus2995@gmail.com>
2024-05-15 15:58:07 +0700

    ACW2-3065: Enh Informasi Nasabah Avras

--- src/components/molecules/specifics/avrast/InformasiNasabah/MainContentComponent/Card.tsx
+++ src/components/molecules/specifics/avrast/InformasiNasabah/MainContentComponent/Card.tsx
@@ -3,11 +3,9 @@ import Image from 'next/image';
 import Link from 'next/link';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { CardPurple } from '../../HubungiKami/MainContentComponent/Card';
 import CONTACTS from '@/assets/images/common/contacts.svg';
 import DOCUMENT_SEARCH from '@/assets/images/common/document-search.svg';
 import PERSON from '@/assets/images/common/person.svg';
-
 
 const mockData = [
   {
@@ -38,29 +36,30 @@ const mockData = [
 
 export const ContentCard = () => {
   return (
-    <div className="mt-[64px] grid grid-cols-3 gap-6">
+    <div className="mt-[64px] grid sm:grid-cols-3 xs:grid-cols-1 gap-[24px]">
       {mockData.map((i) => (
-        <CardPurple key={i.id}>
-          <div className="flex flex-col items-center m-[24px]">
+        <div
+          key={i.id}
+          className="flex flex-col items-center justify-center gap-[24px] px-[24px] pt-[24px] pb-[36px] border border-gray_light border-b-8 border-b-purple_dark rounded-[12px]"
+        >
           <Image
             alt={i.toString()}
             src={i.icon}
             className="w-[100px] h-[100px]"
-            />
-            <p className="text-center font-bold text-[28px] font-karla mt-[24px]">
-              {i.title}
-            </p>
-            <p className="font-opensans text-[16px] text-center mt-[12px]">
-              {i.desc}
-            </p>
+          />
+          <p className="text-center font-bold text-[32px] font-karla">
+            {i.title}
+          </p>
+          <div className="flex flex-col justify-between grow items-center gap-[24px]">
+            <p className="font-opensans text-[16px] text-center">{i.desc}</p>
             <Link
               href={i.link}
-              className="mt-[24px] bg-purple_dark max-w-[260px] w-full text-white rounded-md flex items-center justify-center py-[8px]"
+              className="bg-purple_dark max-w-[260px] w-full text-white rounded-md flex items-center justify-center py-[8px] px-[20px]"
             >
               {i.btn}
             </Link>
           </div>
-        </CardPurple>
+        </div>
       ))}
     </div>
   );
