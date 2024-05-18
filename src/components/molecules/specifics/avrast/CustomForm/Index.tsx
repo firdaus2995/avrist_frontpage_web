bc4ed6f0c8c0058a8dc2aa5aa67afc70c8bc40f0
Firdaus <muhamad.firdaus2995@gmail.com>
2024-05-18 16:01:07 +0700

    ACW2-3185: responsive halaman hubungi kami

--- src/components/molecules/specifics/avrast/CustomForm/Index.tsx
+++ src/components/molecules/specifics/avrast/CustomForm/Index.tsx
@@ -80,17 +80,17 @@ const CustomForm: React.FC<CustomFormProps> = ({
 
     return (
       <div
-        className={`${customFormClassname} flex flex-col self-stretch bg-white p-[36px] gap-[36px] border border-gray_light border-b-8  rounded-xl`}
+        className={`${customFormClassname} flex flex-col self-stretch bg-white gap-[2.25rem] border border-gray_light border-b-8  rounded-xl`}
       >
-        <p className="font-karla font-bold text-[36px] sm:text-[56px]">
+        <p className="font-karla font-bold text-[2.25rem] sm:text-[3.5rem]">
           {title ? title : 'Saya berminat memiliki proteksi ini'}
         </p>
         {type === 'Hubungi Kami' ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-[32px]">
+          <div className="sm:grid sm:grid-cols-2 xs:flex xs:flex-col gap-[2rem]">
             {attributeList?.map((attribute: Attribute, idx) => (
               <div
                 key={attribute.id}
-                className={`pt-4 ${idx === 0 || attribute.fieldType === 'LABEL' ? 'col-span-2' : ''}`}
+                className={`pt-1 ${idx === 0 || attribute.fieldType === 'LABEL' ? 'col-span-2' : ''}`}
               >
                 {attribute.fieldType === 'LABEL' ? (
                   <p>{attribute.name}</p>
@@ -100,7 +100,7 @@ const CustomForm: React.FC<CustomFormProps> = ({
                       {attribute.name} <span className="text-reddist">*</span>
                     </p>
                     {attribute.fieldType === 'RADIO_BUTTON' ? (
-                      <div className="flex flex-row gap-4">
+                      <div className="flex flex-row gap-1">
                         {attribute.value
                           ?.split(';')
                           .map((option, optionIndex) => (
@@ -124,7 +124,7 @@ const CustomForm: React.FC<CustomFormProps> = ({
                         onChange={(e) =>
                           updateFormDataByName(attribute.name, e.target.value)
                         }
-                        className="w-full px-[16px] py-[10px] border border-purple_dark text-purple_dark rounded-md focus:outline-none focus:border-blue-500"
+                        className="w-full px-[1rem] py-[0.625rem] border border-purple_dark text-purple_dark rounded-md focus:outline-none focus:border-blue-500"
                       >
                         <option value={''}>Pilih</option>
                         {attribute.value?.split(',').map((option, idx) => (
@@ -145,7 +145,7 @@ const CustomForm: React.FC<CustomFormProps> = ({
                     ) : attribute.fieldType === 'TEXT_AREA' ? (
                       <div className="flex flex-col justify-end items-end">
                         <textarea
-                          className="w-full px-[16px] py-[10px] border border-gray_light rounded-[14px] text-[14px]"
+                          className="w-full px-[1rem] py-[0.625rem] border border-gray_light rounded-[0.875rem] text-[0.875rem]"
                           placeholder={JSON.parse(attribute.config).placeholder}
                           name={attribute.name}
                           rows={4}
@@ -166,14 +166,14 @@ const CustomForm: React.FC<CustomFormProps> = ({
                             : JSON.parse(attribute.config).max_length)}
                       </div>
                     ) : attribute.name.includes('Telepon') ? (
-                      <div className="flex justify-between gap-[8px]">
+                      <div className="flex justify-between gap-[0.5rem]">
                         <input
-                          className="w-1/5 sm:w-1/5 px-[10px] py-[10px] border border-gray_light rounded-[14px] text-[14px]"
+                          className="w-1/5 sm:w-1/5 px-[0.625rem] py-[0.625rem] border border-gray_light rounded-[0.875rem] text-[0.875rem]"
                           defaultValue={'+62'}
                           readOnly
                         />
                         <input
-                          className="w-4/5 sm:w-4/5 px-[16px] py-[10px] border border-gray_light rounded-[14px] text-[14px]"
+                          className="w-4/5 sm:w-4/5 px-[1rem] py-[0.625rem] border border-gray_light rounded-[0.875rem] text-[0.875rem]"
                           placeholder="Masukan nomor telepon"
                           name={attribute.name}
                           type="number"
@@ -187,7 +187,7 @@ const CustomForm: React.FC<CustomFormProps> = ({
                       </div>
                     ) : (
                       <input
-                        className="w-full px-[16px] py-[10px] border border-gray_light rounded-[14px] text-[14px]"
+                        className="w-full px-[1rem] py-[0.625rem] border border-gray_light rounded-[0.875rem] text-[0.875rem]"
                         placeholder={JSON.parse(attribute.config).placeholder}
                         name={attribute.name}
                         onChange={(e) =>
@@ -201,9 +201,9 @@ const CustomForm: React.FC<CustomFormProps> = ({
             ))}
           </div>
         ) : type === 'Form Saran' ? (
-          <div className="grid grid-cols-1 gap-[32px]">
+          <div className="grid xs:grid-cols-1 sm:grid-cols-2 gap-[2rem]">
             {attributeList?.map((attribute: Attribute) => (
-              <div key={attribute.id} className={`pt-4`}>
+              <div key={attribute.id} className={`pt-1`}>
                 {attribute.fieldType === 'LABEL' ? (
                   <p>{attribute.name}</p>
                 ) : (
@@ -212,7 +212,7 @@ const CustomForm: React.FC<CustomFormProps> = ({
                       {attribute.name} <span className="text-reddist">*</span>
                     </p>
                     {attribute.fieldType === 'RADIO_BUTTON' ? (
-                      <div className="flex flex-row gap-4">
+                      <div className="flex flex-row gap-1">
                         {attribute.value
                           ?.split(';')
                           .map((option, optionIndex) => (
@@ -257,7 +257,7 @@ const CustomForm: React.FC<CustomFormProps> = ({
                     ) : attribute.fieldType === 'TEXT_AREA' ? (
                       <div className="flex flex-col justify-end items-end">
                         <textarea
-                          className="w-full px-[16px] py-[10px] border border-gray_light rounded-[14px] text-[14px]"
+                          className="w-full px-[1rem] py-[0.625rem] border border-gray_light rounded-[0.875rem] text-[0.875rem]"
                           placeholder={JSON.parse(attribute.config).placeholder}
                           name={attribute.name}
                           rows={4}
@@ -278,14 +278,14 @@ const CustomForm: React.FC<CustomFormProps> = ({
                             : JSON.parse(attribute.config).max_length)}
                       </div>
                     ) : attribute.name.includes('Telepon') ? (
-                      <div className="flex justify-between gap-[8px]">
+                      <div className="flex justify-between gap-[0.5rem]">
                         <input
-                          className="w-1/5 sm:w-1/5 px-[10px] py-[10px] border border-gray_light rounded-[14px] text-[14px]"
+                          className="w-1/5 sm:w-1/5 px-[0.625rem] py-[0.625rem] border border-gray_light rounded-[0.875rem] text-[0.875rem]"
                           defaultValue={'+62'}
                           readOnly
                         />
                         <input
-                          className="w-4/5 sm:w-4/5 px-[16px] py-[10px] border border-gray_light rounded-[14px] text-[14px]"
+                          className="w-4/5 sm:w-4/5 px-[1rem] py-[0.625rem] border border-gray_light rounded-[0.875rem] text-[0.875rem]"
                           placeholder="Masukan nomor telepon"
                           name={attribute.name}
                           type="number"
@@ -299,7 +299,7 @@ const CustomForm: React.FC<CustomFormProps> = ({
                       </div>
                     ) : (
                       <input
-                        className="w-full px-[16px] py-[10px] border border-gray_light rounded-[14px] text-[14px]"
+                        className="w-full px-[1rem] py-[0.625rem] border border-gray_light rounded-[0.875rem] text-[0.875rem]"
                         placeholder={JSON.parse(attribute.config).placeholder}
                         name={attribute.name}
                         onChange={(e) =>
@@ -313,10 +313,10 @@ const CustomForm: React.FC<CustomFormProps> = ({
             ))}
           </div>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-[32px]">
-            <div className="flex flex-col gap-[4px]">
+          <div className="grid xs:grid-cols-1 sm:grid-cols-2 gap-[2rem]">
+            <div className="flex flex-col gap-[0.25rem]">
               {leftSide?.map((attribute: Attribute) => (
-                <div key={attribute.id} className="pt-4">
+                <div key={attribute.id} className="pt-1">
                   <p className="font-bold">
                     {attribute.name} <span className="text-reddist">*</span>
                   </p>
@@ -337,7 +337,7 @@ const CustomForm: React.FC<CustomFormProps> = ({
                       ))
                   ) : (
                     <input
-                      className="w-full px-[16px] py-[10px] border border-gray_light rounded-[14px] text-[14px]"
+                      className="w-full px-[1rem] py-[0.625rem] border border-gray_light rounded-[0.875rem] text-[0.875rem]"
                       placeholder={JSON.parse(attribute.config).placeholder}
                       name={attribute.name}
                       onChange={(e) =>
@@ -348,9 +348,9 @@ const CustomForm: React.FC<CustomFormProps> = ({
                 </div>
               ))}
             </div>
-            <div className="flex flex-col gap-[4px]">
+            <div className="flex flex-col gap-[0.25rem]">
               {rightSide?.map((attribute: Attribute) => (
-                <div key={attribute.id} className="pt-4">
+                <div key={attribute.id} className="pt-1">
                   <p className="font-bold">
                     {attribute.name} <span className="text-reddist">*</span>
                   </p>
@@ -370,14 +370,14 @@ const CustomForm: React.FC<CustomFormProps> = ({
                         />
                       ))
                   ) : attribute.name.includes('Telepon') ? (
-                    <div className="flex justify-between gap-[8px]">
+                    <div className="flex justify-between gap-[0.5rem]">
                       <input
-                        className="w-1/5 sm:w-1/5 px-[10px] py-[10px] border border-gray_light rounded-[14px] text-[14px]"
+                        className="w-1/5 sm:w-1/5 px-[0.625rem] py-[0.625rem] border border-gray_light rounded-[0.875rem] text-[0.875rem]"
                         defaultValue={'+62'}
                         readOnly
                       />
                       <input
-                        className="w-4/5 sm:w-4/5 px-[16px] py-[10px] border border-gray_light rounded-[14px] text-[14px]"
+                        className="w-4/5 sm:w-4/5 px-[1rem] py-[0.625rem] border border-gray_light rounded-[0.875rem] text-[0.875rem]"
                         placeholder="Masukan nomor telepon"
                         name={attribute.name}
                         onChange={(e) =>
@@ -387,7 +387,7 @@ const CustomForm: React.FC<CustomFormProps> = ({
                     </div>
                   ) : (
                     <input
-                      className="w-full px-[16px] py-[10px] border border-gray_light rounded-[14px] text-[14px]"
+                      className="w-full px-[1rem] py-[0.625rem] border border-gray_light rounded-[0.875rem] text-[0.875rem]"
                       placeholder={JSON.parse(attribute.config).placeholder}
                       name={attribute.name}
                       onChange={(e) =>
@@ -407,13 +407,13 @@ const CustomForm: React.FC<CustomFormProps> = ({
   return !dataForm ? (
     <>
       <div
-        className={`${customFormClassname} flex flex-col self-stretch bg-white p-[36px] gap-[36px] border border-gray_light border-b-8 rounded-[12px] rounded-b-[8px]`}
+        className={`${customFormClassname} flex flex-col self-stretch bg-white p-[2.25rem] gap-[2.25rem] border border-gray_light border-b-8 rounded-[0.75rem] rounded-b-[0.5rem]`}
       >
-        <p className="font-karla font-bold text-[36px] sm:text-[56px]">
+        <p className="font-karla font-bold text-[2.25rem] sm:text-[3.5rem]">
           Saya berminat memiliki proteksi ini
         </p>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-[32px]">
-          <div className="flex flex-col gap-[4px]">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-[2rem]">
+          <div className="flex flex-col gap-[0.25rem]">
             <p className="font-bold">
               Saya adalah <span className="text-reddist">*</span>
             </p>
@@ -424,63 +424,63 @@ const CustomForm: React.FC<CustomFormProps> = ({
             />
             <Radio id="nasabah" name="tipe_nasabah" label="Nasabah" />
           </div>
-          <div className="flex flex-col gap-[4px]">
+          <div className="flex flex-col gap-[0.25rem]">
             <p className="font-bold">
               Bapak/Ibu <span className="text-reddist">*</span>
             </p>
             <Radio id="bapak" name="jenis_kelamin" label="Bapak" />
             <Radio id="ibu" name="jenis_kelamin" label="Ibu" />
           </div>
-          <div className="flex flex-col gap-[4px]">
+          <div className="flex flex-col gap-[0.25rem]">
             <p className="font-bold">
               Alamat Email <span className="text-reddist">*</span>
             </p>
             <input
-              className="w-full px-[16px] py-[10px] border border-gray_light rounded-[14px] text-[14px]"
+              className="w-full px-[1rem] py-[0.625rem] border border-gray_light rounded-[0.875rem] text-[0.875rem]"
               placeholder="Masukan alamat e-mail Anda"
             />
           </div>
-          <div className="flex flex-col gap-[4px]">
+          <div className="flex flex-col gap-[0.25rem]">
             <p className="font-bold">
               Nama <span className="text-reddist">*</span>
             </p>
             <input
-              className="w-full px-[16px] py-[10px] border border-gray_light rounded-[14px] text-[14px]"
+              className="w-full px-[1rem] py-[0.625rem] border border-gray_light rounded-[0.875rem] text-[0.875rem]"
               placeholder="Masukan nama Anda"
             />
           </div>
-          <div className="flex flex-col gap-[4px]">
+          <div className="flex flex-col gap-[0.25rem]">
             <p className="font-bold">
               Kota <span className="text-reddist">*</span>
             </p>
             <input
-              className="w-full px-[16px] py-[10px] border border-gray_light rounded-[14px] text-[14px]"
+              className="w-full px-[1rem] py-[0.625rem] border border-gray_light rounded-[0.875rem] text-[0.875rem]"
               placeholder="Kota terdekat dari domisili Anda"
             />
           </div>
-          <div className="flex flex-col gap-[4px]">
+          <div className="flex flex-col gap-[0.25rem]">
             <p className="font-bold">
               No Telepon <span className="text-reddist">*</span>
             </p>
-            <div className="flex justify-between gap-[8px]">
+            <div className="flex justify-between gap-[0.5rem]">
               <input
-                className="w-1/5 sm:w-1/5 px-[10px] py-[10px] border border-gray_light rounded-[14px] text-[14px]"
+                className="w-1/5 sm:w-1/5 px-[0.625rem] py-[0.625rem] border border-gray_light rounded-[0.875rem] text-[0.875rem]"
                 defaultValue="+62"
                 readOnly
               />
               <input
-                className="w-4/5 sm:w-4/5 px-[16px] py-[10px] border border-gray_light rounded-[14px] text-[14px]"
+                className="w-4/5 sm:w-4/5 px-[1rem] py-[0.625rem] border border-gray_light rounded-[0.875rem] text-[0.875rem]"
                 placeholder="Masukan nomor telepon"
               />
             </div>
           </div>
         </div>
-        <div className="accent-purple_dark flex flex-row items-start gap-[12px]">
+        <div className="accent-purple_dark flex flex-row items-start gap-[0.75rem]">
           <input
             id="setuju"
             type="checkbox"
             value=""
-            className="mt-[6px] text-purple_dark border-gray_verylight rounded focus:purple_dark focus:ring-2 cursor-pointer"
+            className="mt-[0.375rem] text-purple_dark border-gray_verylight rounded focus:purple_dark focus:ring-2 cursor-pointer"
           />
           <label className="cursor-pointer" htmlFor="setuju">
             Saya setuju memberikan data pribadi Saya kepada Avrist Life
@@ -492,11 +492,11 @@ const CustomForm: React.FC<CustomFormProps> = ({
             Insurance.
           </label>
         </div>
-        <div className="flex flex-col sm:flex-row justify-between items-center gap-[32px]">
+        <div className="flex flex-col sm:flex-row justify-between items-center gap-[2rem]">
           <Image alt="captcha" src={CaptchaPicture} />
           <button
             type="button"
-            className={`${customFormButtonClassname} text-[20px] font-semibold h-[64px] px-[40px] py-[12px] border-1 rounded-[8px]`}
+            className={`${customFormButtonClassname} text-[1.25rem] font-semibold h-[4rem] px-[2.5rem] py-[0.75rem] border-1 rounded-[0.5rem]`}
           >
             <p>Beli Sekarang</p>
           </button>
