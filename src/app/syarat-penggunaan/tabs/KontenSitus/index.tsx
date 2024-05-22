92fd63acd2f5f7b37831ace3aca5838857755b0b
adekferdian <adekferdian@gmail.com>
2024-05-22 16:57:17 +0700

    ACW2-3340: responsive syarat penggunaan

--- src/app/syarat-penggunaan/tabs/KontenSitus/index.tsx
+++ src/app/syarat-penggunaan/tabs/KontenSitus/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const KontenSitus = () => {
   return (
     <div>
-      <p className="font-karla font-bold text-[36px] text-purple_dark mt-[24px]">
+      <p className="font-karla font-bold sm:text-[36px] xs:text-[24px] text-purple_dark mt-[24px]">
         Konten Situs
       </p>
       <p className="font-opensans font-normal text-[20px] text-gray_body mt-[24px]">
