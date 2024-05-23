9f573434792f1feee5d59053f28cb22c011afd61
farizerz <fariz.rizki.ramadhan@gmail.com>
2024-05-23 21:17:59 +0700

    ACW2-3177: Enhancement Detail Klaim

--- src/components/molecules/specifics/avrast/Klaim/InformasiKlaim/index.tsx
+++ src/components/molecules/specifics/avrast/Klaim/InformasiKlaim/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React, { useCallback, useEffect, useState } from 'react';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import Slider from 'react-slick';
 import InfoKlaimTab from './components/InformasiKlaim';
 import PanduanPengajuanTab from './components/PanduanPengajuan';
 
@@ -23,6 +24,16 @@ const InformasiKlaimComponent: React.FC<InformasiKlaimComponentProps> = ({
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const [selected, setSelected] = useState('');
+
+  const sliderTabSettings = {
+    dots: false,
+    infinite: false,
+    arrows: false,
+    centerMode: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
 
   useEffect(() => {
     if (tab !== '') {
@@ -49,21 +60,43 @@ const InformasiKlaimComponent: React.FC<InformasiKlaimComponentProps> = ({
   );
 
   return (
-    <div className="w-full flex flex-col self-stretch items-center justify-center md:px-[8.5rem] xs:px-1 bg-white pt-[6.25rem] pb-[5rem] gap-[4rem]">
-      <div className="flex sm:w-full xs:w-[90%] md:flex-row xs:flex-col gap-4 rounded-lg gap-[0.75rem] flex-wrap mb-10">
-        {data.map((val, idx) => (
-          <div
-            key={idx}
-            role="button"
-            onClick={() => {
-              handleTabClick(val);
-              onChangeBannerImg(idx);
-            }}
-            className={`grow flex p-2 items-center justify-center rounded-lg border border-purple_dark text-[1rem] font-semibold ${selected === val ? 'text-white bg-purple_dark' : 'text-purple_dark bg-white'}`}
-          >
-            {val}
-          </div>
-        ))}
+    <div className="w-full flex flex-col self-stretch items-center justify-center xs:px-[2rem] md:px-[8.5rem] bg-white xs:pt-[3.125rem] xs:pb-[2.25rem] md:pt-[6.25rem] md:pb-[4rem] gap-[4rem]">
+      {/* Tab Desktop */}
+      <div className="w-full xs:hidden md:block">
+        <div className="flex sm:w-full xs:w-[90%] md:flex-row xs:flex-col gap-4 rounded-lg gap-[0.75rem] flex-wrap mb-10">
+          {data.map((val, idx) => (
+            <div
+              key={idx}
+              role="button"
+              onClick={() => {
+                handleTabClick(val);
+                onChangeBannerImg(idx);
+              }}
+              className={`grow flex p-2 items-center justify-center rounded-lg border border-purple_dark text-[1rem] font-semibold ${selected === val ? 'text-white bg-purple_dark' : 'text-purple_dark bg-white'}`}
+            >
+              {val}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Tab Mobile */}
+      <div className="w-[100%] z-20 top-32 md:hidden">
+        <div>
+          <Slider {...sliderTabSettings}>
+            {data.map((val, idx) => (
+              <div key={idx}>
+                <div
+                  role="button"
+                  onClick={() => handleTabClick(val)}
+                  className={`mx-[10px] p-2 border border-purple_dark rounded-lg text-center ${tab === val ? 'bg-purple_dark text-white' : 'text-purple_dark'} font-semibold`}
+                >
+                  {val}
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
       </div>
       {selected === 'Informasi Klaim' && <InfoKlaimTab />}
       {selected === 'Panduan & Pengajuan' && !isSelectedDetail && (
--- src/components/molecules/specifics/avrast/Klaim/ProsesKlaim/index.tsx
+++ src/components/molecules/specifics/avrast/Klaim/ProsesKlaim/index.tsx
@@ -6,7 +6,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Image from 'next/image';
 import HeartIcon from '@/assets/images/avrast/component/panduan-pengajuan/icon-2.svg';
-import CHEVRONRIGHTPURPLE from '@/assets/images/avrast/component/product-section/chevron-right-purple.svg';
+import CHEVRONRIGHTPURPLE from '@/assets/images/common/chevron-right-purple.svg';
 import Button from '@/components/atoms/Button/Button';
 import Icon from '@/components/atoms/Icon';
 import Input from '@/components/atoms/Input';
@@ -19,7 +19,7 @@ import {
 
 const data = [
   {
-    category: 'Asuransi Jiwa Individu',
+    category: 'Asuransi Individu',
     data: [
       'Klaim Manfaat Hidup',
       'Klaim Kematian',
@@ -89,9 +89,10 @@ const ProsesKlaim: React.FC<ProsesKlaimComponentProps> = ({
   const [selectedData, setSelectedData] = useState<any>();
   const [isSelectedData, setIsSelectedData] = useState(false);
   const [search, setSearch] = useState('');
+  const [categoryList, setCategoryList] = useState<string[]>([]);
   const [params, setParams] = useState({
     includeAttributes: 'true',
-    category: data[0].category,
+    category: categoryList[0],
     searchFilter: ''
   });
   const [pagination, setPagination] = useState({
@@ -113,6 +114,22 @@ const ProsesKlaim: React.FC<ProsesKlaimComponentProps> = ({
     setPagination({ ...pagination, currentPage: page });
   };
 
+  const fetchCategory = async () => {
+    try {
+      await handleGetContentCategory('klaim-data', {
+        includeAttributes: 'true'
+      }).then((res) => {
+        setCategoryList(Object.keys(res.data.categoryList));
+        setParams({
+          ...params,
+          category: Object.keys(res.data.categoryList)[0]
+        });
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const fetchData = async () => {
     try {
       const fetchApi = await handleGetContentCategory('klaim-data', params);
@@ -127,13 +144,17 @@ const ProsesKlaim: React.FC<ProsesKlaimComponentProps> = ({
   };
 
   useEffect(() => {
-    fetchData();
-  }, [params]);
+    if (categoryList.length === 0) {
+      fetchCategory();
+    } else {
+      fetchData();
+    }
+  }, [params, categoryList]);
 
   const renderStep = (idx: number) => {
     return (
-      <div className="w-full flex flex-col items-center justify-start py-10 text-left p-[2.25rem] border rounded-[0.75rem] border-t-8 border-t-purple_dark">
-        <h2 className="w-full text-[2.25rem] font-bold mb-6 text-purple_dark">
+      <div className="w-full flex flex-col items-center justify-start p-[2.25rem] text-left border rounded-[0.75rem] border-t-8 border-t-purple_dark">
+        <h2 className="w-full text-[2.25rem] font-bold text-purple_dark font-karla xs:mb-[1.5rem] md:mb-[2.25rem] leading-[2.7rem]">
           {detailData[selectedDetailCategory]}
         </h2>
         {contentData && selectedData && renderDetailStep(idx)}
@@ -145,37 +166,40 @@ const ProsesKlaim: React.FC<ProsesKlaimComponentProps> = ({
     switch (idx) {
       case 0:
         return (
-          <div className="w-full flex flex-col gap-[2.25rem] text-[1.25rem]">
+          <div className="w-full flex flex-col xs:gap-[1.5rem] md:gap-[2.25rem] text-[1.25rem] font-opensans">
             <p
               dangerouslySetInnerHTML={{
                 __html: selectedData.content['tab-1-paragraf-1'].value
               }}
             />
-            <p
-              dangerouslySetInnerHTML={{
-                __html: selectedData.content['tab-1-paragraf-2'].value
-              }}
-            />
-            <div className="grid md:grid-cols-2 xs:grid-cols-1 gap-4">
-              {selectedData.content['tab-1-list'].contentData.map(
-                (val: any, idx: number) =>
-                  val.details[1].value && (
-                    <div
-                      key={idx}
-                      className="flex flex-row gap-2 font-semibold items-center"
-                    >
-                      <Image
-                        src={singleImageTransformer(val.details[0]).imageUrl}
-                        alt={val.details[1].value}
-                        className="w-[2.25rem]"
-                        width={2.25}
-                        height={2.25}
-                      />
-                      <p>{val.details[1].value}</p>
-                    </div>
-                  )
-              )}
+            <div className="flex flex-col gap-[1.25rem]">
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: selectedData.content['tab-1-paragraf-2'].value
+                }}
+              />
+              <div className="grid xs:grid-cols-1 md:grid-cols-2 gap-[2rem]">
+                {selectedData.content['tab-1-list'].contentData.map(
+                  (val: any, idx: number) =>
+                    val.details[1].value && (
+                      <div
+                        key={idx}
+                        className="flex flex-row gap-[0.75rem] font-semibold items-center text-xl"
+                      >
+                        <Image
+                          src={singleImageTransformer(val.details[0]).imageUrl}
+                          alt={val.details[1].value}
+                          className="w-[2.25rem]"
+                          width={2.25}
+                          height={2.25}
+                        />
+                        <p>{val.details[1].value}</p>
+                      </div>
+                    )
+                )}
+              </div>
             </div>
+
             <p
               dangerouslySetInnerHTML={{
                 __html: selectedData.content['tab-1-paragraf-3'].value
@@ -185,13 +209,13 @@ const ProsesKlaim: React.FC<ProsesKlaimComponentProps> = ({
         );
       case 1:
         return (
-          <div className="w-full flex flex-col gap-[2.25rem]">
+          <div className="w-full flex flex-col xs:gap-[1.5rem] md:gap-[1.75rem]">
             {selectedData.content['tab-2-list'].contentData.map(
               (val: any, idx: number) =>
                 val.details[1].value && (
                   <div
                     key={idx}
-                    className="flex flex-row gap-2 text-[1.25rem] font-semibold items-center"
+                    className="flex flex-row gap-[0.75rem] text-[1.25rem] font-semibold items-center font-opensans"
                   >
                     <Image
                       src={singleImageTransformer(val.details[0]).imageUrl}
@@ -219,23 +243,23 @@ const ProsesKlaim: React.FC<ProsesKlaimComponentProps> = ({
                 return (
                   file !== '[]' && (
                     <div
-                      className="flex w-full md:flex-row xs:flex-col gap-[1.5rem] shadow-xl justify-between rounded-[0.75rem] border p-[1.5rem]"
+                      className="flex w-full md:flex-row xs:flex-col gap-[1.5rem] shadow-md justify-between rounded-[0.75rem] border p-[1.5rem]"
                       key={idx}
                     >
                       <div className="flex flex-col gap-2">
                         <p className="text-[1.5rem] font-bold">
                           {val.details[0].value}
                         </p>
-                        <div className="flex flex-row gap-2 text-purple_dark font-medium">
-                          <p className="bg-purple_dark/[0.06] text-[0.875rem] py-[0.25rem] px-[0.5rem]">
+                        <div className="flex flex-row gap-2 text-purple_dark font-semibold font-opensans">
+                          <p className="bg-purple_dark/[0.06] text-sm py-[0.25rem] px-[0.5rem]">
                             {format.toUpperCase()}
                           </p>
                           {/* <p className="p-2 bg-purple_dark/[0.06]">605.59 KB</p> */}
                         </div>
                       </div>
-                      <div className="flex items-center justify-center">
+                      <div className="flex items-center md:justify-center">
                         <div
-                          className="py-[0.5rem] px-[1.25rem] rounded-[0.75rem] text-white font-semibold bg-purple_dark cursor-pointer"
+                          className="py-[0.5rem] px-[2.594rem] rounded-[0.75rem] text-white font-semibold bg-purple_dark cursor-pointer font-opensans"
                           onClick={async () =>
                             await handleDownload(
                               singleImageTransformer(val.details[1]).imageUrl
@@ -256,7 +280,7 @@ const ProsesKlaim: React.FC<ProsesKlaimComponentProps> = ({
         return (
           <div className="w-full flex flex-col gap-4">
             <p
-              className="text-[1.25rem]"
+              className="text-[1.25rem] font-opensans"
               dangerouslySetInnerHTML={{
                 __html: selectedData.content['tab-4-paragraf'].value
               }}
@@ -270,7 +294,7 @@ const ProsesKlaim: React.FC<ProsesKlaimComponentProps> = ({
 
   return (
     <div
-      className={`w-full flex flex-col justify-center gap-[4rem] relative sm:px-[8.5rem] sm:pb-[6.25rem] ${isSelectedData ? 'bg-white' : 'bg-purple_light_bg sm:pt-[5rem]'} rounded-b-[4.0625rem]`}
+      className={`w-full flex flex-col justify-center gap-[4rem] xs:px-[2rem] md:px-[8.5rem] relative ${isSelectedData ? 'bg-white' : 'bg-purple_light_bg sm:pt-[5rem]'} mb-1`}
     >
       {!isSelectedData && (
         <div className="w-full flex flex-col items-center justify-center py-2 text-center">
@@ -284,25 +308,25 @@ const ProsesKlaim: React.FC<ProsesKlaimComponentProps> = ({
         </div>
       )}
 
-      <div className="w-full flex md:flex-row xs:flex-col gap-5 md:px-[5rem] xs:p-[1.25rem]">
-        <div className="w-1/4 xs:hidden md:block">
+      <div className="w-full flex md:flex-row xs:flex-col gap-[4rem]">
+        <div className="xs:hidden md:block">
           <div
             className={`w-full flex flex-col ${isSelectedData ? 'bg-purple_light_bg' : 'bg-white'} rounded-[0.75rem]`}
           >
-            {!isSelectedData
-              ? data.map((val, idx) => (
+            {!isSelectedData && categoryList.length > 0
+              ? categoryList.map((val, idx) => (
                   <div
                     key={idx}
                     role="button"
                     onClick={() => {
                       setParams({
                         ...params,
-                        category: val.category
+                        category: val
                       });
                     }}
-                    className={`${idx === 0 && 'rounded-tl-[0.75rem]'} ${idx + 1 === data.length && 'rounded-bl-[0.75rem]'} ${params.category !== val.category && 'opacity-50'} border-l-8 border-l-purple_dark p-4 text-[1.125rem] font-bold text-purple_dark`}
+                    className={`${idx === 0 && 'rounded-tl-[0.75rem]'} ${idx + 1 === data.length && 'rounded-bl-[0.75rem]'} ${params.category !== val && 'opacity-50'} border-l-8 border-l-purple_dark p-4 text-[1.125rem] font-bold text-purple_dark`}
                   >
-                    {val.category}
+                    {val}
                   </div>
                 ))
               : detailData?.map((val, idx) => (
@@ -317,69 +341,14 @@ const ProsesKlaim: React.FC<ProsesKlaimComponentProps> = ({
                         onChangeBannerImg(2);
                       }
                     }}
-                    className={`${idx === 0 && 'rounded-tl-[0.75rem]'} ${idx + 1 === detailData.length && 'rounded-bl-[0.75rem]'} ${selectedDetailCategory !== idx && 'opacity-50'} border-l-8 border-l-purple_dark p-4 font-semibold text-purple_dark`}
+                    className={`${idx === 0 && 'rounded-tl-[0.75rem]'} ${idx + 1 === detailData.length && 'rounded-bl-[0.75rem]'} ${selectedDetailCategory !== idx && 'opacity-50'} border-l-8 border-l-purple_dark p-4 font-bold text-purple_dark text-lg`}
                   >
                     {val}
                   </div>
                 ))}
           </div>
         </div>
-        <div className="w-full xs:block md:hidden">
-          <div
-            className={`w-full p-2 flex flex-col ${isSelectedData ? 'bg-purple_light_bg' : 'bg-white'} rounded-[0.75rem] border-l-8 border-l-purple_dark font-semibold text-purple_dark`}
-          >
-            {!isSelectedData ? (
-              <select
-                id="selected-categories"
-                onChange={(e) => {
-                  const selectedValue = e.target.value;
-                  setParams({
-                    ...params,
-                    category: selectedValue
-                  });
-                }}
-                className="p-2"
-              >
-                {data.map((val, idx) => (
-                  <option
-                    key={idx}
-                    selected={val.category === params.category}
-                    value={val.category}
-                    className="w-[80%]"
-                  >
-                    {val.category}
-                  </option>
-                ))}
-              </select>
-            ) : (
-              <select
-                id="selected-detail-categories"
-                onChange={(e) => {
-                  const idx = e.target.selectedIndex;
-                  setSelectedDetailCategory(idx);
-                  if (idx > 0) {
-                    onChangeBannerImg(idx + 2);
-                  } else {
-                    onChangeBannerImg(2);
-                  }
-                }}
-                className="p-2 bg-purple_light_bg"
-              >
-                {detailData.map((val, idx) => (
-                  <option
-                    key={idx}
-                    selected={val === selectedData}
-                    value={val}
-                    className="w-[80%]"
-                  >
-                    {val}
-                  </option>
-                ))}
-              </select>
-            )}
-          </div>
-        </div>
-        <div className="md:w-3/4 xs:w-full flex flex-col gap-4">
+        <div className="xs:w-full flex flex-col gap-[2.25rem]">
           {!isSelectedData ? (
             <div className="flex flex-row gap-2">
               <Input
@@ -400,23 +369,85 @@ const ProsesKlaim: React.FC<ProsesKlaimComponentProps> = ({
               />
             </div>
           ) : (
-            <div className="flex md:flex-row xs:flex-col xs:divide-y md:divide-y-0 gap-4 justify-between border rounded-[0.75rem] p-4 text-purple_dark font-semibold">
-              <div className="flex flex-row items-center gap-2">
-                <Image src={HeartIcon} alt="heart-icon" className="w-7" />
-                <p className="text-[1.5rem] font-bold">{params.category}</p>
+            <div className="flex flex-col gap-[2.25rem]">
+              <div className="flex md:flex-row xs:flex-col xs:divide-y md:divide-y-0 gap-4 justify-between border rounded-[0.75rem] p-4 text-purple_dark font-semibold">
+                <div className="flex flex-row items-center gap-2">
+                  <Image src={HeartIcon} alt="heart-icon" className="w-7" />
+                  <p className="text-[1.5rem] font-bold font-karla">
+                    {params.category}
+                  </p>
+                </div>
+                <div
+                  className="flex flex-row items-center gap-2 text-[1.25rem] font-semibold"
+                  role="button"
+                  onClick={() => {
+                    setSelectedData('');
+                    setIsSelectedData(false);
+                    onSelectDetail(false);
+                    onChangeBannerImg(1);
+                  }}
+                >
+                  <Icon name="chevronLeft" color="purple_dark" />
+                  Kembali
+                </div>
               </div>
-              <div
-                className="flex flex-row items-center gap-2 text-[1.25rem] font-semibold"
-                role="button"
-                onClick={() => {
-                  setSelectedData('');
-                  setIsSelectedData(false);
-                  onSelectDetail(false);
-                  onChangeBannerImg(1);
-                }}
-              >
-                <Icon name="chevronLeft" color="purple_dark" />
-                Kembali
+              <div className="w-full flex xs:block md:hidden">
+                <div
+                  className={`w-full p-2 flex flex-col ${isSelectedData ? 'bg-purple_light_bg' : 'bg-white'} rounded-[0.75rem] border-l-8 border-l-purple_dark font-bold text-purple_dark text-lg`}
+                >
+                  {!isSelectedData ? (
+                    <select
+                      id="selected-categories"
+                      onChange={(e) => {
+                        const selectedValue = e.target.value;
+                        setParams({
+                          ...params,
+                          category: selectedValue
+                        });
+                      }}
+                      className="p-2"
+                    >
+                      {data.map((val, idx) => (
+                        <option
+                          key={idx}
+                          selected={val.category === params.category}
+                          value={val.category}
+                          className="w-[80%]"
+                        >
+                          {val.category}
+                        </option>
+                      ))}
+                    </select>
+                  ) : (
+                    <select
+                      id="selected-detail-categories"
+                      onChange={(e) => {
+                        const idx = e.target.selectedIndex;
+                        setSelectedDetailCategory(idx);
+                        if (idx > 0) {
+                          onChangeBannerImg(idx + 2);
+                        } else {
+                          onChangeBannerImg(2);
+                        }
+                      }}
+                      className="p-2 bg-purple_light_bg whitespace-normal"
+                    >
+                      {detailData.map((val, idx) => (
+                        <option
+                          key={idx}
+                          selected={val === selectedData}
+                          value={val}
+                          className="w-[80%]"
+                        >
+                          {val}
+                        </option>
+                      ))}
+                    </select>
+                  )}
+                </div>
+              </div>
+              <div className="md:text-[3.5rem] xs:text-[1.5rem] font-bold font-karla">
+                {selectedData.title}
               </div>
             </div>
           )}
@@ -488,11 +519,7 @@ const ProsesKlaim: React.FC<ProsesKlaimComponentProps> = ({
               </div>
             </div>
           )}
-          {selectedData && (
-            <div className="md:text-[3.5rem] xs:text-[1.5rem] font-bold">
-              {selectedData.title}
-            </div>
-          )}
+
           {selectedData && renderStep(selectedDetailCategory)}
         </div>
       </div>
