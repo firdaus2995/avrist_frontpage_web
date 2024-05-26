aa6907db85628c44a03cc01b78d0b1dde28ed56e
Fariz Rizki Ramadhan <fariz.rizki.ramadhan@gmail.com>
2024-05-26 19:20:24 +0700

    ACW2-3399: Fix Href on Klaim Layanan Avrist Syariah

--- src/app/avrist-syariah/tabs/KlaimDanLayanan/index.tsx
+++ src/app/avrist-syariah/tabs/KlaimDanLayanan/index.tsx
@@ -19,49 +19,44 @@ const KlaimDanLayanan = () => {
           {
             cardIcon: GreenReceipt,
             cardTitle: 'Kelola Klaim',
-            cardBody:
-              'Informasi seputar panduan dan layanan klaim',
+            cardBody: 'Informasi seputar panduan dan layanan klaim',
             cardButtonText: 'Panduan Klaim',
             href: '/klaim-layanan/klaim?tab=Panduan+%26+Pengajuan'
           },
           {
             cardIcon: GreenContact,
             cardTitle: 'Kelola Perlindungan Anda',
-            cardBody:
-              'Kelola informasi seputar Polis Anda ',
+            cardBody: 'Kelola informasi seputar Polis Anda ',
             cardButtonText: 'Informasi Nasabah',
             href: '/klaim-layanan/layanan/kelola-polis'
           },
           {
             cardIcon: GreenHospital,
             cardTitle: 'Rumah Sakit Rekanan',
-            cardBody:
-              'Jaringan Rekanan Rumah Sakit Avrist Assurance',
+            cardBody: 'Jaringan Rekanan Rumah Sakit Avrist Assurance',
             cardButtonText: 'Cari Lokasi',
             href: '/klaim-layanan/layanan?tab=Rumah+Sakit+Rekanan'
           },
           {
             cardIcon: GreenForm,
             cardTitle: 'Formulir dan Buku Panduan',
-            cardBody:
-              'Akses dokumen cepat, untuk kebutuhan Anda',
+            cardBody: 'Akses dokumen cepat, untuk kebutuhan Anda',
             cardButtonText: 'Unduh dan Baca',
             href: '/klaim-layanan/layanan?tab=Formulir+%26+Buku+Panduan'
           },
           {
             cardIcon: GreenPaper,
             cardTitle: 'Penanganan Pengaduan',
-            cardBody:
-              'Mekanisme Penanganan Pengaduan',
+            cardBody: 'Mekanisme Penanganan Pengaduan',
             cardButtonText: 'Selengkapnya',
             href: 'https://avrist.com/Prosedur%20Singkat%20Pelayanan%20Nasabah%20Avrist%20_updated.pdf'
           },
           {
             cardIcon: GreenHelpDesk,
             cardTitle: 'Bantuan',
-            cardBody:
-              'Kami di sini siap membantu Anda',
-            cardButtonText: 'Hubungi Kami'
+            cardBody: 'Kami di sini siap membantu Anda',
+            cardButtonText: 'Hubungi Kami',
+            href: '/hubungi-kami'
           }
         ]}
         cardClassname="border-b-syariah_green"
