39ac7de75fe92cc974314cdd049838b53de73608
adekferdian <adekferdian@gmail.com>
2024-06-07 15:25:41 +0700

    ACW2-3397: Responsive Halaman under construction

--- src/app/under-construction/page.tsx
+++ src/app/under-construction/page.tsx
@@ -5,21 +5,21 @@ export default function NotFound() {
   return (
     <div>
       <div className="my-[96px] w-full flex flex-col items-center">
-        <p className="font-karla font-bold text-[80px] text-purple_verylight">
+        <p className="font-karla font-extrabold md:text-[80px] xs:text-[55px] text-purple_verylight text-center">
           Under Construction
         </p>
-        <div className="w-[420px] text-center">
+        <div className="w-[420px] text-center xs:max-md:px-[50px]">
           <p className="font-karla font-bold text-[24px]">
             Halaman dalam proses pembuatan
           </p>
-          <p className="font-opensans text-[16px] mt-[12px]">
+          <p className="font-opensans text-[16px] mt-[12px] font-normal">
             Sepertinya halaman yang Anda cari belum tersedia. Silakan periksa
             kembali URL atau kembali ke halaman utama kami.
           </p>
         </div>
         <Link
           href="/"
-          className="bg-purple_dark text-white font-opensans font-semibold text-[20px] rounded-lg py-[18px] px-[40px] mt-[36px]"
+          className="bg-purple_dark text-white font-opensans font-semibold text-[20px] rounded-lg md:py-[18px] xs:py-[12px] px-[40px] mt-[36px]"
         >
           Kembali Ke Beranda
         </Link>
