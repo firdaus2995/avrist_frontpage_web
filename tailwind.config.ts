7302e2dcef79159f4209d8ec6dbcdbcee4b9a1ad
farizerz <fariz.rizki.ramadhan@gmail.com>
2024-06-23 15:02:13 +0700

    ACW2-3837: Change Global Layout Max Width

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,8 @@ const config: Config = {
       md: '768px',
       lg: '1024px',
       xl: '1280px',
-      '2xl': '1536px'
+      '2xl': '1536px',
+      '3xl': '120rem'
     },
     extend: {
       borderWidth: {
