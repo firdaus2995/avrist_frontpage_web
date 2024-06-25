adb21e8684296b268a48397310bf0b04854cfabd
Firdaus <muhamad.firdaus2995@gmail.com>
2024-06-26 02:17:27 +0700

    ACW2-3882: Bugfix letter spacing nama section (desktop & responsive): Halaman klaim & layanan / Rumah sakit rekanan

--- src/components/molecules/specifics/avrast/RSRekanan/Content/index.tsx
+++ src/components/molecules/specifics/avrast/RSRekanan/Content/index.tsx
@@ -82,7 +82,7 @@ const Content = () => {
         />
 
         <section className="w-full flex flex-col items-center text-center">
-          <h1 className="font-karla xs:text-[2.25rem] sm:text-[3.5rem] text-purple_dark font-medium">
+          <h1 className="font-karla xs:text-[2.25rem] sm:text-[3.5rem] text-purple_dark font-medium xs:-tracking-[1.44px] sm:-tracking-[2.56px]">
             Jaringan Rekanan Rumah Sakit Avrist Assurance
           </h1>
           <h2 className="font-karla xs:text-[1.5rem] sm:text-[2.25rem]">
