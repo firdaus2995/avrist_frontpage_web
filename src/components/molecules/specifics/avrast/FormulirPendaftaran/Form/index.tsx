dfe00c2f938b4920a478cd143148e6f020d36ba5
Firdaus <muhamad.firdaus2995@gmail.com>
2024-06-26 02:18:41 +0700

    ACW2-3884: Bugfix margin antara section: Halaman Klaim & Layanan / Formulir dan Buku Panduan

--- src/components/molecules/specifics/avrast/FormulirPendaftaran/Form/index.tsx
+++ src/components/molecules/specifics/avrast/FormulirPendaftaran/Form/index.tsx
@@ -74,7 +74,7 @@ const Form = () => {
 
   return (
     <div className="w-full bg-purple_dark">
-      <div className="bg-white flex flex-col pt-[3.125rem] md:pt-[6.25rem] px-[2rem] md:px-[8.5rem] pb-[1.625rem] sm:gap-[4rem] xs:gap-[2.25rem]">
+      <div className="bg-white flex flex-col pt-[5rem] md:pt-[6.25rem] px-[2rem] md:px-[8.5rem] pb-[3rem] sm:gap-[4rem] xs:gap-[2.25rem]">
         <ButtonMenu
           buttonList={[
             'Informasi Nasabah',
