55240455af1c9481506cadb90f72d0f07dd4ab6d
Firdaus <muhamad.firdaus2995@gmail.com>
2024-06-26 02:15:20 +0700

    ACW2-3892: Bugfix margin antara section: Halaman Klaim & Layanan / Informasi nasabah / Penanganan Pengaduan

--- src/components/molecules/specifics/avrast/PenangananPengaduan/MainContent.tsx
+++ src/components/molecules/specifics/avrast/PenangananPengaduan/MainContent.tsx
@@ -218,7 +218,7 @@ export const MainContent = ({
   return (
     <div className="w-full flex flex-col">
       <div className="bg-white flex flex-col">
-        <div className="bg-white w-full flex flex-col sm:pt-[100px] sm:pb-[26px] sm:px-[136px] sm:gap-[64px] xs:p-4 xs:gap-[36px]">
+        <div className="bg-white w-full flex flex-col sm:pt-[5rem] sm:pb-[3rem] sm:px-[136px] sm:gap-[64px] xs:p-4 xs:gap-[36px]">
           <ButtonMenu />
           <Content />
           {videoData && <VideoInformation pageVideoData={videoData} />}
