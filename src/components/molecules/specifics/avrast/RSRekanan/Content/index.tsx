e40dabbe0126e50147bf077f351be9a12fabbb08
Firdaus <muhamad.firdaus2995@gmail.com>
2024-06-27 21:53:41 +0700

    ACW2-3881: Bugfix margin antara section: Halaman Klaim & Layanan / Rumah sakit rekanan

--- src/components/molecules/specifics/avrast/RSRekanan/Content/index.tsx
+++ src/components/molecules/specifics/avrast/RSRekanan/Content/index.tsx
@@ -97,7 +97,7 @@ const Content = () => {
 
   return (
     <div className="w-full bg-purple_dark">
-      <div className="bg-white flex flex-col sm:pt-[5rem] xs:pt-[3.125rem] px-[2rem] md:px-[8.5rem] pb-[3rem] sm:gap-[4rem] xs:gap-[2.25rem]">
+      <div className="bg-white flex flex-col sm:pt-[5rem] xs:pt-[3.125rem] px-[2rem] md:px-[8.5rem] pb-[3rem] sm:gap-[5rem] xs:gap-[2.25rem]">
         <ButtonMenu
           buttonList={[
             'Informasi Nasabah',
