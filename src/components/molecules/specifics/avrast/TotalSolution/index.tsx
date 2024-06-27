a6e90b13d6cd48237fb4fe114aeebc9fb3532a71
adekferdian <adekferdian@gmail.com>
2024-06-27 20:23:50 +0700

    ACW2-3958: Bugfix add auto slider mengapa avrist

--- src/components/molecules/specifics/avrast/TotalSolution/index.tsx
+++ src/components/molecules/specifics/avrast/TotalSolution/index.tsx
@@ -113,9 +113,7 @@ const TotalSolution = () => {
     infinite: false,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000
+    slidesToScroll: 1
   };
 
   const renderCard = (
