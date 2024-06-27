31d79e85956b40071ae9f62e27c6bb8679b5540a
Firdaus <muhamad.firdaus2995@gmail.com>
2024-06-27 16:57:47 +0700

    ACW2-3895: Bugfix margin antara section: Halaman Klaim & Layanan / Informasi nasabah / Panduan Polis / Panduan Polis Detail

--- src/components/molecules/specifics/avrast/KelolaPolis/MainContent.tsx
+++ src/components/molecules/specifics/avrast/KelolaPolis/MainContent.tsx
@@ -38,7 +38,7 @@ export const MainContent = ({
 
   return (
     <div className="w-full flex flex-col">
-      <div className="bg-white w-full flex flex-col sm:pt-[5rem] sm:pb-[3rem] sm:px-[136px] sm:gap-[64px] xs:p-4 xs:gap-[36px]">
+      <div className="bg-white w-full flex flex-col sm:pt-[5rem] sm:pb-[3rem] sm:px-[136px] sm:gap-[5rem] xs:p-4 xs:gap-[36px]">
         <ButtonMenu />
         <Content />
         {videoData && (
