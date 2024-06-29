fdcfffeeb9edc0549d551cc462c1c70275d1dcc9
Syahrul Romadhon <rmdhn.syahrul@gmail.com>
2024-06-29 16:36:55 +0700

    ACW2-0000: fixing padding layanan nasabah

--- src/components/molecules/specifics/avrast/LayananNasabah/index.tsx
+++ src/components/molecules/specifics/avrast/LayananNasabah/index.tsx
@@ -131,7 +131,7 @@ const LayananNasabah = () => {
   );
 
   return (
-    <div className="flex flex-col self-stretch items-center justify-center py-32 gap-16 bg-white rounded-t-[65px] relative">
+    <div className="flex flex-col self-stretch items-center justify-center pt-[5rem] pb-[100px] gap-16 bg-white rounded-t-[65px] relative">
       <div className="w-full absolute z-20 top-2 h-20 bg-white rounded-t-[65px]"></div>
       <div className="w-full flex flex-row absolute z-10 top-0 h-20 rounded-t-[65px]">
         <div className="w-1/5 h-full bg-green_border rounded-tl-[65px]"></div>
