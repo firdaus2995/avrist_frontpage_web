f760660dbbab5e551e0bb5c694aa8d9df8732c47
adekferdian <adekferdian@gmail.com>
2024-07-19 14:07:33 +0700

    ACW2-4207: Implement get location saat buka halaman home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BannerAvrast from '@/components/molecules/specifics/avrast/Banner/Banner';
 import CompanySection from '@/components/molecules/specifics/avrast/CompanySection';
 import LayananNasabah from '@/components/molecules/specifics/avrast/LayananNasabah';
@@ -8,6 +8,30 @@ import TotalSolution from '@/components/molecules/specifics/avrast/TotalSolution
 
 const Avrist = () => {
   const [popupUrl, setPopupUrl] = useState<string>('');
+
+  useEffect(() => {
+    const requestLocation = () => {
+      if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const location: any = {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude
+            };
+            if (typeof window !== 'undefined') {
+              localStorage.setItem('location', JSON.stringify(location));
+            }
+          },
+          () => {
+            if (typeof window !== 'undefined') {
+              localStorage.removeItem('location');
+            }
+          }
+        );
+      }
+    };
+    requestLocation();
+  }, []);
   return (
     <div className="flex flex-col items-center justify-center bg-purple_light_bg">
       <BannerAvrast onPopUpURL={(val) => setPopupUrl(val)} />
