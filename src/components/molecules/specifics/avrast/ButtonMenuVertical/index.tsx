08ec466cf5263faa5da4cff51bc6ab8068d0b9a7
Firdaus <muhamad.firdaus2995@gmail.com>
2024-07-30 15:20:48 +0700

    ACW2-4344: Bugfix dropdownlist pada halaman panduan pengajuan detail

--- src/components/molecules/specifics/avrast/ButtonMenuVertical/index.tsx
+++ src/components/molecules/specifics/avrast/ButtonMenuVertical/index.tsx
@@ -46,17 +46,14 @@ const ButtonMenuVertical: React.FC<IButtonMenuVertical> = ({
 
       <div className="md:hidden w-full h-full bg-white rounded-xl relative">
         <span
-          className="flex flex-row justify-between items-center cursor-pointer pr-4"
+          className={`flex flex-row justify-between items-center cursor-pointer pr-4 px-5 py-2 border-l-8 border-l-purple_dark ${isOpen ? 'rounded-t-xl' : 'rounded-xl'}`}
           onClick={() => {
             setIsOpen(!isOpen);
           }}
         >
           <div className="flex flex-row gap-4 items-center">
-            <div
-              className={`w-[6px] h-[49px] bg-purple_dark ${isOpen ? 'rounded-tl-xl' : 'rounded-l-xl'}`}
-            />
             <p
-              className={`font-opensanspro text-[18px] font-bold text-purple_dark leading-[25.2px]`}
+              className={`font-opensanspro text-[18px] font-bold text-purple_dark leading-[25.2px] pr-4`}
             >
               {item[selected]?.title}
             </p>
@@ -72,7 +69,7 @@ const ButtonMenuVertical: React.FC<IButtonMenuVertical> = ({
               (i, index) =>
                 index !== selected && (
                   <span
-                    className="flex flex-row justify-between items-center cursor-pointer pr-4"
+                    className={`flex flex-row justify-between items-center cursor-pointer pr-4 px-5 py-2 border-l-8 border-l-purple_dark ${index === item.length - 1 ? 'rounded-b-xl' : ''}`}
                     key={index}
                     onClick={() => {
                       i.onClick ? i.onClick() : null;
@@ -81,11 +78,8 @@ const ButtonMenuVertical: React.FC<IButtonMenuVertical> = ({
                     }}
                   >
                     <div className="flex flex-row gap-4 items-center">
-                      <div
-                        className={`w-[6px] h-[49px] ${index === selected ? 'bg-purple_dark' : 'bg-purple_mediumlight'} ${index === item.length ? 'rounded-bl-xl' : ''}`}
-                      />
                       <p
-                        className={`text-md sm:text-[18px] font-bold ${i.color ? i.color : index === selected ? 'text-purple_dark' : 'text-purple_mediumlight'}`}
+                        className={`text-md sm:text-[18px] font-bold pr-4 ${i.color ? i.color : index === selected ? 'text-purple_dark' : 'text-purple_mediumlight'}`}
                       >
                         {index !== selected && i.title}
                       </p>
