759ed16dce5b1d3b8f4f99d79fd2aa315f3976a8
Ryugujji <ryugujji@Ryugujjis-MacBook-Pro.local>
2024-10-25 17:12:26 +0800

    ACW2-1: Add suspense component for fallback

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,9 +63,11 @@ export default function RootLayout({
       <body
         className={`${openSans.variable} ${karla.variable} w-full max-w-screen-2xl 3xl:max-w-screen-3xl mx-auto`}
       >
+        <Suspense>
         <Header />
         {children}
         <Footer />
+        </Suspense>
       </body>
       <Suspense>
         <ClientSideScrollRestorer />
